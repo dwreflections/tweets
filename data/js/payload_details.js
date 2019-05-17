82e0b5c46e6d1966de0d8341b8c80e329069b5fa
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-05-17 02:17:31 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4733,
-	"created_at": "Thu May 16 01:17:30 +0000 2019",
+	"tweets": 4735,
+	"created_at": "Fri May 17 01:17:30 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 16 17:28:48 +0000 2019",
+		"id": 1129076245025710100,
+		"id_str": "1129076245025710083",
+		"full_text": "One for @thomas_jesson 👇 (cheers for the link @StewartWilliam6)\n\nThe princess who thought she was made of glass https://t.co/W9eh6LBS1b via @BBC_Culture",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thomas_jesson",
+					"name": "Tom Jesson",
+					"id": 3091527256,
+					"id_str": "3091527256",
+					"indices": [
+						8,
+						22
+					]
+				},
+				{
+					"screen_name": "StewartWilliam6",
+					"name": "Stewart Williams",
+					"id": 553780066,
+					"id_str": "553780066",
+					"indices": [
+						46,
+						62
+					]
+				},
+				{
+					"screen_name": "BBC_Culture",
+					"name": "BBC Culture",
+					"id": 1207654748,
+					"id_str": "1207654748",
+					"indices": [
+						140,
+						152
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/W9eh6LBS1b",
+					"expanded_url": "http://www.bbc.com/culture/story/20190516-the-princess-who-thought-she-was-made-of-glass?ocid=ww.social.link.twitter",
+					"display_url": "bbc.com/culture/story/…",
+					"indices": [
+						112,
+						135
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams ✨",
+			"screen_name": "beardedPT",
+			"location": "Nottingham, England",
+			"description": "Student Physiotherapist ▪️ Professional Committee at the Chartered Society of Physiotherapy▪️ #150leaders▪️Healthcare Leadership ▪️Social Media 🔁 Dissertation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1507,
+			"friends_count": 2875,
+			"listed_count": 14,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 11865,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4741,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1091028176107696128/nL51pB0L_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1091028176107696128/nL51pB0L_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "One for @thomas_jesson 👇 (cheers for the link @StewartWilliam6)\n\nThe princess who thought she was made of glass https://t.co/W9eh6LBS1b via @BBC_Culture"
+	},
+	{
+		"created_at": "Thu May 16 17:21:08 +0000 2019",
+		"id": 1129032117726384100,
+		"id_str": "1129074312886050816",
+		"full_text": "2 weeks until my non-assessed physio elective placement that I need to complete in order to graduate! Is ANYONE in Bath/Bristol able to help out and host it? The placement is 3 weeks long and I can ensure some cake provisions 😋⤵️ very very very stressed https://t.co/vJ7LeRLMz5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1129032111900495900,
+					"id_str": "1129032111900495874",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/D6sguVHWkAI66Y6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6sguVHWkAI66Y6.jpg",
+					"url": "https://t.co/vJ7LeRLMz5",
+					"display_url": "pic.twitter.com/vJ7LeRLMz5",
+					"expanded_url": "https://twitter.com/JoeThomasPhysio/status/1129032117726384128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1129032111900495900,
+					"id_str": "1129032111900495874",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/D6sguVHWkAI66Y6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6sguVHWkAI66Y6.jpg",
+					"url": "https://t.co/vJ7LeRLMz5",
+					"display_url": "pic.twitter.com/vJ7LeRLMz5",
+					"expanded_url": "https://twitter.com/JoeThomasPhysio/status/1129032117726384128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 989454211778797600,
+			"id_str": "989454211778797568",
+			"name": "Joe Thomas",
+			"screen_name": "JoeThomasPhysio",
+			"location": "Bath",
+			"description": "University of Nottingham 3rd year physiotherapy student 🎓 | Welsh academics rugby player 🏉 | Bath 🏬 | CSP student 👨🏼‍🎓| Career aim = Bath Rugby Physio 🎯",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 229,
+			"friends_count": 1111,
+			"listed_count": 0,
+			"created_at": "Thu Apr 26 10:40:42 +0000 2018",
+			"favourites_count": 814,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 538,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125842518418055168/RsiAOnvS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125842518418055168/RsiAOnvS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/989454211778797568/1548105905",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "7d7bdec12d2549d4",
+			"url": "https://api.twitter.com/1.1/geo/id/7d7bdec12d2549d4.json",
+			"place_type": "city",
+			"name": "Nottingham",
+			"full_name": "Nottingham, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-1.2501363,
+							52.921473
+						],
+						[
+							-1.094396,
+							52.921473
+						],
+						[
+							-1.094396,
+							53.017965
+						],
+						[
+							-1.2501363,
+							53.017965
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26,
+		"favorite_count": 10,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "2 weeks until my non-assessed physio elective placement that I need to complete in order to graduate! Is ANYONE in Bath/Bristol able to help out and host it? The placement is 3 weeks long and I can ensure some cake provisions 😋⤵️ very very very stressed https://t.co/vJ7LeRLMz5"
+	},
+	{
 		"created_at": "Wed May 15 19:23:15 +0000 2019",
 		"id": 1128741655304077300,
 		"id_str": "1128742659697328130",
