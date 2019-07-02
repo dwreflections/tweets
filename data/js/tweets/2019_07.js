57ba53758aed346d1bb447d86b47513162fc7883
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-07-02 02:17:31 +0100

    Added 3 tweets

---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,427 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Mon Jul 01 21:02:09 +0000 2019",
+		"id": 1145793311766134800,
+		"id_str": "1145799778288185351",
+		"full_text": "Warm energy from a room full of #150leaders family. Notes to share:\n- always think- where am I? What more can I do?\n- build alignment&amp;alliances\n- learn through relationships not hierarchy #inittogether #futureNHS #StNProject",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "150leaders",
+					"indices": [
+						32,
+						43
+					]
+				},
+				{
+					"text": "inittogether",
+					"indices": [
+						192,
+						205
+					]
+				},
+				{
+					"text": "futureNHS",
+					"indices": [
+						206,
+						216
+					]
+				},
+				{
+					"text": "StNProject",
+					"indices": [
+						217,
+						228
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 569576220,
+			"id_str": "569576220",
+			"name": "Becky Crisp",
+			"screen_name": "rebbeccrisp",
+			"location": "Birmingham, England",
+			"description": "#hellomynameisbecky 〰️2nd Yr BCU undergraduate Nurse 〰️NATTER PROBLEM: nattering to students about sexual health 〰️ #150leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 428,
+			"friends_count": 1274,
+			"listed_count": 4,
+			"created_at": "Wed May 02 23:00:07 +0000 2012",
+			"favourites_count": 1760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 710,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFE779",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1048933657774383104/TWFyLtpI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1048933657774383104/TWFyLtpI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/569576220/1540848393",
+			"profile_link_color": "1C1B19",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E74A69",
+			"profile_text_color": "FF936C",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 18,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Warm energy from a room full of #150leaders family. Notes to share:\n- always think- where am I? What more can I do?\n- build alignment&amp;alliances\n- learn through relationships not hierarchy #inittogether #futureNHS #StNProject"
+	},
+	{
+		"created_at": "Mon Jul 01 10:30:39 +0000 2019",
+		"id": 1145640853681164300,
+		"id_str": "1145640853681164288",
+		"full_text": "@Shaun_LDNurse @councilofdeans @150Leaders Have the best day! #150Leaders",
+		"truncated": false,
+		"display_text_range": [
+			43,
+			73
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "150Leaders",
+					"indices": [
+						62,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Shaun_LDNurse",
+					"name": "Shaun Williams",
+					"id": 901055791515435000,
+					"id_str": "901055791515435008",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "councilofdeans",
+					"name": "Council of Deans",
+					"id": 278442312,
+					"id_str": "278442312",
+					"indices": [
+						15,
+						30
+					]
+				},
+				{
+					"screen_name": "150Leaders",
+					"name": "#150Leaders",
+					"id": 1139187039151280100,
+					"id_str": "1139187039151280128",
+					"indices": [
+						31,
+						42
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1145639359888142300,
+		"in_reply_to_status_id_str": "1145639359888142336",
+		"in_reply_to_user_id": 901055791515435000,
+		"in_reply_to_user_id_str": "901055791515435008",
+		"in_reply_to_screen_name": "Shaun_LDNurse",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams ✨",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Student Physiotherapist ▪️ Professional Committee at the Chartered Society of Physiotherapy▪️ #150leaders▪️Healthcare Leadership ▪️Social Media 🔁 Dissertation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1571,
+			"friends_count": 2977,
+			"listed_count": 15,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 12590,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4984,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1131305717426728960/RpYbIkpj_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1131305717426728960/RpYbIkpj_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Shaun_LDNurse @councilofdeans @150Leaders Have the best day! #150Leaders"
+	},
+	{
+		"created_at": "Mon Jul 01 09:27:24 +0000 2019",
+		"id": 1145401045683724300,
+		"id_str": "1145624936293314560",
+		"full_text": "WIN A TICKET to Reforming MSK Practice 2019 by #TheBigRs\n\nImprove your chances by replying to this post with suggestions of what MSK events of the future should be!\n\nFull entry details attached\nBook now to guarantee\nWinner reimbursed\nhttps://t.co/43Qw6egdzw https://t.co/UzpjGB9TI8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TheBigRs",
+					"indices": [
+						47,
+						56
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/43Qw6egdzw",
+					"expanded_url": "http://reform.physio/conference",
+					"display_url": "reform.physio/conference",
+					"indices": [
+						234,
+						257
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1145401039727804400,
+					"id_str": "1145401039727804416",
+					"indices": [
+						258,
+						281
+					],
+					"media_url": "http://pbs.twimg.com/media/D-VILVJWwAAAEYy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-VILVJWwAAAEYy.jpg",
+					"url": "https://t.co/UzpjGB9TI8",
+					"display_url": "pic.twitter.com/UzpjGB9TI8",
+					"expanded_url": "https://twitter.com/TPMPodcast/status/1145401045683724289/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1242,
+							"h": 1728,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 489,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 862,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1145401039727804400,
+					"id_str": "1145401039727804416",
+					"indices": [
+						258,
+						281
+					],
+					"media_url": "http://pbs.twimg.com/media/D-VILVJWwAAAEYy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-VILVJWwAAAEYy.jpg",
+					"url": "https://t.co/UzpjGB9TI8",
+					"display_url": "pic.twitter.com/UzpjGB9TI8",
+					"expanded_url": "https://twitter.com/TPMPodcast/status/1145401045683724289/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1242,
+							"h": 1728,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 489,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 862,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1914280350,
+			"id_str": "1914280350",
+			"name": "Physio Matters",
+			"screen_name": "TPMPodcast",
+			"location": "United Kingdom",
+			"description": "Confronting Bad Ideas With Good Ones since 2013 | 1.5 Million Downloads | Brain-child of @JackAChew | Tweets from team #TPMP |",
+			"url": "https://t.co/qWMSyCCcZA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qWMSyCCcZA",
+							"expanded_url": "http://chewshealth.co.uk/the-physio-matters-podcast/",
+							"display_url": "chewshealth.co.uk/the-physio-mat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17376,
+			"friends_count": 325,
+			"listed_count": 100,
+			"created_at": "Sat Sep 28 14:30:49 +0000 2013",
+			"favourites_count": 9396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17271,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1080205037303984129/sUcVPZvR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1080205037303984129/sUcVPZvR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1914280350/1546418358",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 20,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "WIN A TICKET to Reforming MSK Practice 2019 by #TheBigRs\n\nImprove your chances by replying to this post with suggestions of what MSK events of the future should be!\n\nFull entry details attached\nBook now to guarantee\nWinner reimbursed\nhttps://t.co/43Qw6egdzw https://t.co/UzpjGB9TI8"
+	}
+]
