226f1e962c99500c1d9f3943600c6478278cc247
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-07-06 02:17:30 +0100

    Added 1 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,119 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Fri Jul 05 11:58:19 +0000 2019",
+		"id": 1147112470580011000,
+		"id_str": "1147112470580011008",
+		"full_text": "@PeterHoma @AdamSewellJones @GregAllenUK Excellent to see , as you have almost came full circle with your earlier career ambitions in childhood. I look forward to catching up work in the future. Best of luck with the new role.",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			226
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PeterHoma",
+					"name": "Peter Homa",
+					"id": 373838723,
+					"id_str": "373838723",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "AdamSewellJones",
+					"name": "Adam Sewell-Jones",
+					"id": 270907637,
+					"id_str": "270907637",
+					"indices": [
+						11,
+						27
+					]
+				},
+				{
+					"screen_name": "GregAllenUK",
+					"name": "Greg Allen FRSA",
+					"id": 1222303776,
+					"id_str": "1222303776",
+					"indices": [
+						28,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1146835238292734000,
+		"in_reply_to_status_id_str": "1146835238292733953",
+		"in_reply_to_user_id": 373838723,
+		"in_reply_to_user_id_str": "373838723",
+		"in_reply_to_screen_name": "PeterHoma",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Student Physiotherapist ▪️ Professional Committee at the Chartered Society of Physiotherapy▪️ #150leaders▪️Healthcare Leadership ▪️Social Media 🔁 Dissertation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1577,
+			"friends_count": 2987,
+			"listed_count": 15,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 12684,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4996,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146043716387442688/PkPpaozN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146043716387442688/PkPpaozN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@PeterHoma @AdamSewellJones @GregAllenUK Excellent to see , as you have almost came full circle with your earlier career ambitions in childhood. I look forward to catching up work in the future. Best of luck with the new role."
+	},
+	{
 		"created_at": "Thu Jul 04 22:52:34 +0000 2019",
 		"id": 1146914726196256800,
 		"id_str": "1146914726196256770",
