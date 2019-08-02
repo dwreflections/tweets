f8f7f7cef3fa1c39279611d26ebddcf99104eea0
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-08-02 02:17:31 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5083,
-	"created_at": "Thu Aug 01 01:17:31 +0000 2019",
+	"tweets": 5084,
+	"created_at": "Fri Aug 02 01:17:30 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_08.js
@@ -0,0 +1,96 @@
+Grailbird.data.tweets_2019_08 = 
+[
+	{
+		"created_at": "Thu Aug 01 12:32:21 +0000 2019",
+		"id": 1156905507682181000,
+		"id_str": "1156905507682181120",
+		"full_text": "@LeylsTurk As a fountain pen fan I might have to take you up on that advice! 👍🖋",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeylsTurk",
+					"name": "Dr Leyla Türkoğlu",
+					"id": 792749293153157100,
+					"id_str": "792749293153157121",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1156456849564192800,
+		"in_reply_to_status_id_str": "1156456849564192768",
+		"in_reply_to_user_id": 792749293153157100,
+		"in_reply_to_user_id_str": "792749293153157121",
+		"in_reply_to_screen_name": "LeylsTurk",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Rotational  Physiotherapist - Professional Committee Member at the Chartered Society of Physiotherapy - #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1612,
+			"friends_count": 2570,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 13283,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5142,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LeylsTurk As a fountain pen fan I might have to take you up on that advice! 👍🖋"
+	}
+]
