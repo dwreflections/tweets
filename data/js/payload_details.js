08f86ac94bbcab1f6ebcc7a7d70799cb00d6b687
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-08-04 02:17:30 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5087,
-	"created_at": "Sat Aug 03 01:17:29 +0000 2019",
+	"tweets": 5090,
+	"created_at": "Sun Aug 04 01:17:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,430 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 03 21:38:49 +0000 2019",
+		"id": 1157767802796875800,
+		"id_str": "1157767802796875776",
+		"full_text": "@TPMPodcast @consultantPT @thomas_jesson @JackAChew A must listen!! 🕺",
+		"truncated": false,
+		"display_text_range": [
+			52,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TPMPodcast",
+					"name": "Physio Matters",
+					"id": 1914280350,
+					"id_str": "1914280350",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "consultantPT",
+					"name": "Uzo Ehiogu",
+					"id": 1039521456,
+					"id_str": "1039521456",
+					"indices": [
+						12,
+						25
+					]
+				},
+				{
+					"screen_name": "thomas_jesson",
+					"name": "Tom Jesson",
+					"id": 3091527256,
+					"id_str": "3091527256",
+					"indices": [
+						26,
+						40
+					]
+				},
+				{
+					"screen_name": "JackAChew",
+					"name": "Jack Chew",
+					"id": 868558446,
+					"id_str": "868558446",
+					"indices": [
+						41,
+						51
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1157740008243191800,
+		"in_reply_to_status_id_str": "1157740008243191808",
+		"in_reply_to_user_id": 1914280350,
+		"in_reply_to_user_id_str": "1914280350",
+		"in_reply_to_screen_name": "TPMPodcast",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Rotational  Physiotherapist - Professional Committee Member at the Chartered Society of Physiotherapy - #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1614,
+			"friends_count": 2577,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 13315,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5150,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@TPMPodcast @consultantPT @thomas_jesson @JackAChew A must listen!! 🕺"
+	},
+	{
+		"created_at": "Sat Aug 03 21:38:28 +0000 2019",
+		"id": 1157740008243191800,
+		"id_str": "1157767716452876288",
+		"full_text": "Out Tomorrow morning\n@thomas_jesson discussing radicular pain, radiculopathy and sciatica!\n\nSomething for everyone from students to ESPs in this one! https://t.co/dcSfmBy6jO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thomas_jesson",
+					"name": "Tom Jesson",
+					"id": 3091527256,
+					"id_str": "3091527256",
+					"indices": [
+						21,
+						35
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1157739999183458300,
+					"id_str": "1157739999183458304",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/EBEeZTGXUAAQc0E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBEeZTGXUAAQc0E.jpg",
+					"url": "https://t.co/dcSfmBy6jO",
+					"display_url": "pic.twitter.com/dcSfmBy6jO",
+					"expanded_url": "https://twitter.com/TPMPodcast/status/1157740008243191808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1539,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 902,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157739999183458300,
+					"id_str": "1157739999183458304",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/EBEeZTGXUAAQc0E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBEeZTGXUAAQc0E.jpg",
+					"url": "https://t.co/dcSfmBy6jO",
+					"display_url": "pic.twitter.com/dcSfmBy6jO",
+					"expanded_url": "https://twitter.com/TPMPodcast/status/1157740008243191808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1539,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 902,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1914280350,
+			"id_str": "1914280350",
+			"name": "Physio Matters",
+			"screen_name": "TPMPodcast",
+			"location": "United Kingdom",
+			"description": "Confronting Bad Ideas With Good Ones since 2013 | Brain-child of @JackAChew | Tweets by team #TPMP | #TheBigRs tickets: https://t.co/cVbRgwX1ua",
+			"url": "https://t.co/qWMSyCCcZA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qWMSyCCcZA",
+							"expanded_url": "http://chewshealth.co.uk/the-physio-matters-podcast/",
+							"display_url": "chewshealth.co.uk/the-physio-mat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/cVbRgwX1ua",
+							"expanded_url": "http://reform.physio/conference/",
+							"display_url": "reform.physio/conference/",
+							"indices": [
+								120,
+								143
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 17653,
+			"friends_count": 329,
+			"listed_count": 100,
+			"created_at": "Sat Sep 28 14:30:49 +0000 2013",
+			"favourites_count": 9430,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17416,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1080205037303984129/sUcVPZvR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1080205037303984129/sUcVPZvR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1914280350/1564732346",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 62,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Out Tomorrow morning\n@thomas_jesson discussing radicular pain, radiculopathy and sciatica!\n\nSomething for everyone from students to ESPs in this one! https://t.co/dcSfmBy6jO"
+	},
+	{
+		"created_at": "Sat Aug 03 09:29:19 +0000 2019",
+		"id": 1157543333184622600,
+		"id_str": "1157584221696868352",
+		"full_text": "BBC News - Woman, 55, sets John O'Groats to Land's End running record https://t.co/xPqYwCzXyE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xPqYwCzXyE",
+					"expanded_url": "https://www.bbc.co.uk/news/uk-49212792",
+					"display_url": "bbc.co.uk/news/uk-492127…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 570967836,
+			"id_str": "570967836",
+			"name": "Sharon Gayter",
+			"screen_name": "SGayter",
+			"location": "Guisborough",
+			"description": "Extreme ultra runner, author, runs anywhere, any distance and any weather.  Multiple world record holder, represented GB for 18 years at 100km and 24 hrs.",
+			"url": "https://t.co/I5glow8LRT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/I5glow8LRT",
+							"expanded_url": "http://www.sharongayter.com",
+							"display_url": "sharongayter.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 946,
+			"friends_count": 184,
+			"listed_count": 12,
+			"created_at": "Fri May 04 14:42:14 +0000 2012",
+			"favourites_count": 789,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2199697913/sharon_G_minimised_4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2199697913/sharon_G_minimised_4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/570967836/1515183585",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 63,
+		"favorite_count": 376,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BBC News - Woman, 55, sets John O'Groats to Land's End running record https://t.co/xPqYwCzXyE"
+	},
+	{
 		"created_at": "Fri Aug 02 21:24:09 +0000 2019",
 		"id": 1157325983562834000,
 		"id_str": "1157401724233179136",
