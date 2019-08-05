b457b08ed8e94f17dbee91f70ac251cbdb810266
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-08-05 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5090,
-	"created_at": "Sun Aug 04 01:17:29 +0000 2019",
+	"tweets": 5091,
+	"created_at": "Mon Aug 05 01:17:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,117 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 04 10:03:32 +0000 2019",
+		"id": 1157955217402605600,
+		"id_str": "1157955217402605570",
+		"full_text": "@Niki_South @DanielKlapaukh Much like my notes 📝 📝 📝 \n🤣 #WarAndPeace",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			68
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WarAndPeace",
+					"indices": [
+						56,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Niki_South",
+					"name": "Niki S",
+					"id": 765870819385966600,
+					"id_str": "765870819385966593",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "DanielKlapaukh",
+					"name": "Daniel Klapaukh",
+					"id": 591517762,
+					"id_str": "591517762",
+					"indices": [
+						12,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1157951429291774000,
+		"in_reply_to_status_id_str": "1157951429291773953",
+		"in_reply_to_user_id": 765870819385966600,
+		"in_reply_to_user_id_str": "765870819385966593",
+		"in_reply_to_screen_name": "Niki_South",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Rotational  Physiotherapist - Professional Committee Member at the Chartered Society of Physiotherapy - #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1614,
+			"friends_count": 2579,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 13322,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5152,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152707284063182849/PILIH2Mc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Niki_South @DanielKlapaukh Much like my notes 📝 📝 📝 \n🤣 #WarAndPeace"
+	},
+	{
 		"created_at": "Sat Aug 03 21:38:49 +0000 2019",
 		"id": 1157767802796875800,
 		"id_str": "1157767802796875776",
