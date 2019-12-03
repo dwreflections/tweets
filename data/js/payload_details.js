cbe19181dc2eb4f3c758862bcd650df2dceb1328
dwreflections <50467496+dwreflections@users.noreply.github.com>
2019-12-03 01:17:31 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5393,
-	"created_at": "Sat Nov 30 01:17:32 +0000 2019",
+	"tweets": 5395,
+	"created_at": "Tue Dec 03 01:17:30 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,243 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 02 22:50:14 +0000 2019",
+		"id": 1201556844403208200,
+		"id_str": "1201634707819188224",
+		"full_text": "An amazing tribute from Jack's father - what an incredible young man, and politicians should respect and try to uphold the principles he lived by. Jack would be livid his death has been used to further an agenda of hate' | Dave Merritt https://t.co/AnIBNzF5Wb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AnIBNzF5Wb",
+					"expanded_url": "https://www.theguardian.com/uk-news/2019/dec/02/jack-merritt-london-bridge-attack-dave-merritt?CMP=share_btn_tw",
+					"display_url": "theguardian.com/uk-news/2019/d…",
+					"indices": [
+						236,
+						259
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19493482,
+			"id_str": "19493482",
+			"name": "Kath Thacker",
+			"screen_name": "kaththacker",
+			"location": "",
+			"description": "Mum of boys + Otto a Hungarian Wirehaired Vizsla. Advanced Physiotherapy Practitioner in Paediatric MSK/Orthopaedics in NHS @KHFTPaedPhysio",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 350,
+			"friends_count": 714,
+			"listed_count": 10,
+			"created_at": "Sun Jan 25 16:24:38 +0000 2009",
+			"favourites_count": 1407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3215,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/977588962146639877/LIhvLpPQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/977588962146639877/LIhvLpPQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19493482/1521910734",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 10,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "An amazing tribute from Jack's father - what an incredible young man, and politicians should respect and try to uphold the principles he lived by. Jack would be livid his death has been used to further an agenda of hate' | Dave Merritt https://t.co/AnIBNzF5Wb"
+	},
+	{
+		"created_at": "Mon Dec 02 13:10:19 +0000 2019",
+		"id": 1201488768106152000,
+		"id_str": "1201488768106151936",
+		"full_text": "@BoardDan @laurarathbone @thecsp @CSPLondon ☝️ @nicklivadas",
+		"truncated": false,
+		"display_text_range": [
+			44,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BoardDan",
+					"name": "Dan Board",
+					"id": 1643746188,
+					"id_str": "1643746188",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "laurarathbone",
+					"name": "Laura Rathbone🥦🌱✌",
+					"id": 32357102,
+					"id_str": "32357102",
+					"indices": [
+						10,
+						24
+					]
+				},
+				{
+					"screen_name": "thecsp",
+					"name": "Chartered Society of Physiotherapy",
+					"id": 138098880,
+					"id_str": "138098880",
+					"indices": [
+						25,
+						32
+					]
+				},
+				{
+					"screen_name": "CSPLondon",
+					"name": "Physiotherapy",
+					"id": 251678811,
+					"id_str": "251678811",
+					"indices": [
+						33,
+						43
+					]
+				},
+				{
+					"screen_name": "nicklivadas",
+					"name": "Nick Livadas",
+					"id": 375320758,
+					"id_str": "375320758",
+					"indices": [
+						47,
+						59
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1201430266448924700,
+		"in_reply_to_status_id_str": "1201430266448924673",
+		"in_reply_to_user_id": 1643746188,
+		"in_reply_to_user_id_str": "1643746188",
+		"in_reply_to_screen_name": "BoardDan",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Physiotherapist - Professional Committee Member at the Chartered Society of Physiotherapy (https://t.co/Bwu7xtI10V) - Social Media - #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Bwu7xtI10V",
+							"expanded_url": "http://bit.ly/2b3pt",
+							"display_url": "bit.ly/2b3pt",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1741,
+			"friends_count": 2768,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 14892,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5519,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1184179216990003200/QKs-uz_y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1184179216990003200/QKs-uz_y_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@BoardDan @laurarathbone @thecsp @CSPLondon ☝️ @nicklivadas"
+	}
+]
