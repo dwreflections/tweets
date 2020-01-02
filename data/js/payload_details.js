a6618b9bd6e44866175b45243362de5c92f42db6
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-01-02 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5425,
-	"created_at": "Tue Dec 31 01:17:29 +0000 2019",
+	"tweets": 5426,
+	"created_at": "Thu Jan 02 01:17:29 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,228 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Wed Jan 01 18:02:37 +0000 2020",
+		"id": 1212433961483489300,
+		"id_str": "1212433961483489281",
+		"full_text": "@exerciseworks @UniofNottingham @UoN_Pharmacy @UoNPhysio @fimo18 @RogerKerry1 @kirstyhyndes @benedsmith @TaylorAlanJ @marionfwalker @worldheartfed @BJSM_BMJ @u21news Yes Ann! &amp; happy new year 🥳 🥂",
+		"truncated": false,
+		"display_text_range": [
+			166,
+			199
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "exerciseworks",
+					"name": "Exercise Works!",
+					"id": 256921454,
+					"id_str": "256921454",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "UniofNottingham",
+					"name": "Uni of Nottingham",
+					"id": 30196565,
+					"id_str": "30196565",
+					"indices": [
+						15,
+						31
+					]
+				},
+				{
+					"screen_name": "UoN_Pharmacy",
+					"name": "UoNPharmacy",
+					"id": 607266196,
+					"id_str": "607266196",
+					"indices": [
+						32,
+						45
+					]
+				},
+				{
+					"screen_name": "UoNPhysio",
+					"name": "UoN Physiotherapy",
+					"id": 3020424189,
+					"id_str": "3020424189",
+					"indices": [
+						46,
+						56
+					]
+				},
+				{
+					"screen_name": "fimo18",
+					"name": "Dr Fiona Moffatt",
+					"id": 508806750,
+					"id_str": "508806750",
+					"indices": [
+						57,
+						64
+					]
+				},
+				{
+					"screen_name": "RogerKerry1",
+					"name": "Roger Kerry",
+					"id": 396832258,
+					"id_str": "396832258",
+					"indices": [
+						65,
+						77
+					]
+				},
+				{
+					"screen_name": "kirstyhyndes",
+					"name": "kirsty hyndes",
+					"id": 203287084,
+					"id_str": "203287084",
+					"indices": [
+						78,
+						91
+					]
+				},
+				{
+					"screen_name": "benedsmith",
+					"name": "Ben Smith",
+					"id": 22659623,
+					"id_str": "22659623",
+					"indices": [
+						92,
+						103
+					]
+				},
+				{
+					"screen_name": "TaylorAlanJ",
+					"name": "Alan J Taylor",
+					"id": 412127340,
+					"id_str": "412127340",
+					"indices": [
+						104,
+						116
+					]
+				},
+				{
+					"screen_name": "marionfwalker",
+					"name": "Prof Marion Walker",
+					"id": 907162501497880600,
+					"id_str": "907162501497880576",
+					"indices": [
+						117,
+						131
+					]
+				},
+				{
+					"screen_name": "worldheartfed",
+					"name": "World Heart Federation",
+					"id": 83809282,
+					"id_str": "83809282",
+					"indices": [
+						132,
+						146
+					]
+				},
+				{
+					"screen_name": "BJSM_BMJ",
+					"name": "BJSM",
+					"id": 138753057,
+					"id_str": "138753057",
+					"indices": [
+						147,
+						156
+					]
+				},
+				{
+					"screen_name": "u21news",
+					"name": "Universitas 21",
+					"id": 325563534,
+					"id_str": "325563534",
+					"indices": [
+						157,
+						165
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1212364325207380000,
+		"in_reply_to_status_id_str": "1212364325207379968",
+		"in_reply_to_user_id": 256921454,
+		"in_reply_to_user_id_str": "256921454",
+		"in_reply_to_screen_name": "exerciseworks",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Physiotherapist - Professional Committee Member at the Chartered Society of Physiotherapy (https://t.co/Bwu7xtI10V) - Social Media - #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Bwu7xtI10V",
+							"expanded_url": "http://bit.ly/2b3pt",
+							"display_url": "bit.ly/2b3pt",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1757,
+			"friends_count": 2779,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 15129,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5559,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1184179216990003200/QKs-uz_y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1184179216990003200/QKs-uz_y_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@exerciseworks @UniofNottingham @UoN_Pharmacy @UoNPhysio @fimo18 @RogerKerry1 @kirstyhyndes @benedsmith @TaylorAlanJ @marionfwalker @worldheartfed @BJSM_BMJ @u21news Yes Ann! &amp; happy new year 🥳 🥂"
+	}
+]
