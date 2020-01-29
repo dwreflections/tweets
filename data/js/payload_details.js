b719b94bdbae6c3c8144a6b9885517c02404f293
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-01-29 01:17:31 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5471,
-	"created_at": "Sun Jan 26 01:17:29 +0000 2020",
+	"tweets": 5472,
+	"created_at": "Wed Jan 29 01:17:30 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,286 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 28 22:12:08 +0000 2020",
+		"id": 1222260227204178000,
+		"id_str": "1222281229728387075",
+		"full_text": "💥Annual #CAHPO20 Awards are NOW open for entries💥\n\n👉Individual #AHPs or #AHP teams can be nominated or self-nominate\n\n👉9 categories to choose from\n\n👉entries close 17 April 2020\n\n💥Take a look 👉 https://t.co/EZRKWU8Vea https://t.co/EhBPd5UWgA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CAHPO20",
+					"indices": [
+						8,
+						16
+					]
+				},
+				{
+					"text": "AHPs",
+					"indices": [
+						63,
+						68
+					]
+				},
+				{
+					"text": "AHP",
+					"indices": [
+						72,
+						76
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EZRKWU8Vea",
+					"expanded_url": "https://www.england.nhs.uk/ahp/chief-allied-health-professions-officer-awards-2017/",
+					"display_url": "england.nhs.uk/ahp/chief-alli…",
+					"indices": [
+						193,
+						216
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1222260222342942700,
+					"id_str": "1222260222342942722",
+					"indices": [
+						217,
+						240
+					],
+					"media_url": "http://pbs.twimg.com/media/EPZXMkfXUAIYNBX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPZXMkfXUAIYNBX.jpg",
+					"url": "https://t.co/EhBPd5UWgA",
+					"display_url": "pic.twitter.com/EhBPd5UWgA",
+					"expanded_url": "https://twitter.com/SuzanneRastrick/status/1222260227204177922/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "2364573882",
+									"screen_name": "WeAHPs",
+									"name": "WeAHPs"
+								},
+								{
+									"type": "user",
+									"user_id": "720682698805800960",
+									"screen_name": "AHPs4PH",
+									"name": "AHPs4PH"
+								},
+								{
+									"type": "user",
+									"user_id": "551426814",
+									"screen_name": "NHSEngland",
+									"name": "NHS England and NHS Improvement"
+								}
+							]
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1222260222342942700,
+					"id_str": "1222260222342942722",
+					"indices": [
+						217,
+						240
+					],
+					"media_url": "http://pbs.twimg.com/media/EPZXMkfXUAIYNBX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPZXMkfXUAIYNBX.jpg",
+					"url": "https://t.co/EhBPd5UWgA",
+					"display_url": "pic.twitter.com/EhBPd5UWgA",
+					"expanded_url": "https://twitter.com/SuzanneRastrick/status/1222260227204177922/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "2364573882",
+									"screen_name": "WeAHPs",
+									"name": "WeAHPs"
+								},
+								{
+									"type": "user",
+									"user_id": "720682698805800960",
+									"screen_name": "AHPs4PH",
+									"name": "AHPs4PH"
+								},
+								{
+									"type": "user",
+									"user_id": "551426814",
+									"screen_name": "NHSEngland",
+									"name": "NHS England and NHS Improvement"
+								}
+							]
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1596992540,
+			"id_str": "1596992540",
+			"name": "Suzanne Rastrick",
+			"screen_name": "SuzanneRastrick",
+			"location": "England",
+			"description": "Chief Allied Health Professions Officer (CAHPO) for England @NHSEngland & @NHSImprovement #AHPsintoAction #DigitalAHPs #AHPLeadership | NED in housing sector",
+			"url": "https://t.co/D4UEMbVmju",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/D4UEMbVmju",
+							"expanded_url": "https://www.england.nhs.uk/ahp/",
+							"display_url": "england.nhs.uk/ahp/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13284,
+			"friends_count": 680,
+			"listed_count": 91,
+			"created_at": "Mon Jul 15 23:19:04 +0000 2013",
+			"favourites_count": 52096,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23407,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1184579419127668742/6ULPHKXT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1184579419127668742/6ULPHKXT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1596992540/1571260717",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 29,
+		"favorite_count": 41,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "💥Annual #CAHPO20 Awards are NOW open for entries💥\n\n👉Individual #AHPs or #AHP teams can be nominated or self-nominate\n\n👉9 categories to choose from\n\n👉entries close 17 April 2020\n\n💥Take a look 👉 https://t.co/EZRKWU8Vea https://t.co/EhBPd5UWgA"
+	},
+	{
 		"created_at": "Sat Jan 25 13:57:36 +0000 2020",
 		"id": 1221069611233357800,
 		"id_str": "1221069611233357824",
