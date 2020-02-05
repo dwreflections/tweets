f8df49655ad54d3df286f7db7e21d33ac2c255e3
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-02-05 01:17:30 +0000

    Added 1 tweets

---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,159 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Tue Feb 04 18:04:18 +0000 2020",
+		"id": 1224612022127669200,
+		"id_str": "1224755573536755713",
+		"full_text": "Look out for #AHP Armed Forces case studies coming March 2020 Aim to encourage former/current Armed Forces personnel to look at a career as an AHP Inspiring stories from cross section of AHP professions @teamtouchdesign @NHSEArmedForces @BeverleyHarden @SuzanneRastrick @poppytea",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AHP",
+					"indices": [
+						13,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "teamtouchdesign",
+					"name": "Touch Design",
+					"id": 175411546,
+					"id_str": "175411546",
+					"indices": [
+						203,
+						219
+					]
+				},
+				{
+					"screen_name": "NHSEArmedForces",
+					"name": "NHSE Armed Forces",
+					"id": 2831648813,
+					"id_str": "2831648813",
+					"indices": [
+						220,
+						236
+					]
+				},
+				{
+					"screen_name": "BeverleyHarden",
+					"name": "Beverley Harden",
+					"id": 1641141318,
+					"id_str": "1641141318",
+					"indices": [
+						237,
+						252
+					]
+				},
+				{
+					"screen_name": "SuzanneRastrick",
+					"name": "Suzanne Rastrick",
+					"id": 1596992540,
+					"id_str": "1596992540",
+					"indices": [
+						253,
+						269
+					]
+				},
+				{
+					"screen_name": "poppytea",
+					"name": "Denise Baker",
+					"id": 368541768,
+					"id_str": "368541768",
+					"indices": [
+						270,
+						279
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 735562813146533900,
+			"id_str": "735562813146533888",
+			"name": "Paul_Chapman09",
+			"screen_name": "PaulChapman09",
+			"location": "Sheffield, England",
+			"description": "Health Education England National Programme Manager AHPs and Chartered Physiotherapist DSA (CSP) Views my own",
+			"url": "https://t.co/SgtYeFmbMd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SgtYeFmbMd",
+							"expanded_url": "https://uk.linkedin.com/in/paul-chapman-b7717156",
+							"display_url": "uk.linkedin.com/in/paul-chapma…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1480,
+			"friends_count": 589,
+			"listed_count": 11,
+			"created_at": "Wed May 25 20:06:55 +0000 2016",
+			"favourites_count": 4420,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1195070914762878978/fKTW7F_d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1195070914762878978/fKTW7F_d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/735562813146533888/1524681171",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 25,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Look out for #AHP Armed Forces case studies coming March 2020 Aim to encourage former/current Armed Forces personnel to look at a career as an AHP Inspiring stories from cross section of AHP professions @teamtouchdesign @NHSEArmedForces @BeverleyHarden @SuzanneRastrick @poppytea"
+	}
+]
