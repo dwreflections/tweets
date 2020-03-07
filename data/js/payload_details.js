bf81f06d83db331384002161412810ad1fd7d002
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-03-07 01:17:31 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5529,
-	"created_at": "Fri Mar 06 01:17:29 +0000 2020",
+	"tweets": 5530,
+	"created_at": "Sat Mar 07 01:17:30 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 06 16:23:59 +0000 2020",
+		"id": 1235964354572562400,
+		"id_str": "1235964354572562434",
+		"full_text": "@CrouchPhysio @thecsp @shoiab1 ⬆️\n▪️check out: https://t.co/jvRXl6XqaN #CSPLDP",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			78
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CSPLDP",
+					"indices": [
+						71,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CrouchPhysio",
+					"name": "Lewis Crouch",
+					"id": 1132599615998578700,
+					"id_str": "1132599615998578688",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "thecsp",
+					"name": "Chartered Society of Physiotherapy",
+					"id": 138098880,
+					"id_str": "138098880",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"screen_name": "shoiab1",
+					"name": "Shoiab",
+					"id": 173186820,
+					"id_str": "173186820",
+					"indices": [
+						22,
+						30
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jvRXl6XqaN",
+					"expanded_url": "https://www.csp.org.uk/workplace/equality-diversity/diversity-networks",
+					"display_url": "csp.org.uk/workplace/equa…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1235927765536444400,
+		"in_reply_to_status_id_str": "1235927765536444421",
+		"in_reply_to_user_id": 1132599615998578700,
+		"in_reply_to_user_id_str": "1132599615998578688",
+		"in_reply_to_screen_name": "CrouchPhysio",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire, England",
+			"description": "Physiotherapist ▪️Professional Committee Member at the Chartered Society of Physiotherapy (https://t.co/Bwu7xtI10V) ▪️ #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Bwu7xtI10V",
+							"expanded_url": "http://bit.ly/2b3pt",
+							"display_url": "bit.ly/2b3pt",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1877,
+			"friends_count": 2936,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 15794,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234189090494525441/ZriLbdW0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234189090494525441/ZriLbdW0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1445373889",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@CrouchPhysio @thecsp @shoiab1 ⬆️\n▪️check out: https://t.co/jvRXl6XqaN #CSPLDP"
+	},
+	{
 		"created_at": "Thu Mar 05 22:57:05 +0000 2020",
 		"id": 1235700892156014600,
 		"id_str": "1235700892156014593",
