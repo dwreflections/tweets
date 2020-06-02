8d7ac72c2d15df7127acb8e3dfec185efb34aca2
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-06-02 02:17:31 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5782,
-	"created_at": "Fri May 29 01:17:31 +0000 2020",
+	"tweets": 5783,
+	"created_at": "Tue Jun 02 01:17:30 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,337 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 18:04:59 +0000 2020",
+		"id": 1267517513572782000,
+		"id_str": "1267517513572782082",
+		"full_text": "✨Leading The Way ✨\n⬇️Check out these brilliant #AHP leaders ⬇️\n\nGreat to see @PuppyFlanagan @nhumilton @Vicky_553 have case studies 👍 #150Leaders https://t.co/W3HMrsDTDu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AHP",
+					"indices": [
+						47,
+						51
+					]
+				},
+				{
+					"text": "150Leaders",
+					"indices": [
+						134,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PuppyFlanagan",
+					"name": "Nick Flanagan",
+					"id": 380458298,
+					"id_str": "380458298",
+					"indices": [
+						77,
+						91
+					]
+				},
+				{
+					"screen_name": "nhumilton",
+					"name": "Nhu Milton 🇻🇳 🇺🇸",
+					"id": 1021293705200918500,
+					"id_str": "1021293705200918528",
+					"indices": [
+						92,
+						102
+					]
+				},
+				{
+					"screen_name": "Vicky_553",
+					"name": "Victoria Hain",
+					"id": 2273556668,
+					"id_str": "2273556668",
+					"indices": [
+						103,
+						113
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/W3HMrsDTDu",
+					"expanded_url": "https://twitter.com/150leaders/status/1267410815084146689",
+					"display_url": "twitter.com/150leaders/sta…",
+					"indices": [
+						146,
+						169
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🌈",
+			"screen_name": "beardedPT",
+			"location": "Rugeley, England",
+			"description": "Physiotherapist 🦠Professional Committee Member at the Chartered Society of Physiotherapy (https://t.co/Bwu7xtI10V) 🦠 #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Bwu7xtI10V",
+							"expanded_url": "http://bit.ly/2b3pt",
+							"display_url": "bit.ly/2b3pt",
+							"indices": [
+								90,
+								113
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2038,
+			"friends_count": 3209,
+			"listed_count": 16,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 17048,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5994,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1259230694187958276/46x3Y3jv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1259230694187958276/46x3Y3jv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1587218106",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "52041af7b8001313",
+			"url": "https://api.twitter.com/1.1/geo/id/52041af7b8001313.json",
+			"place_type": "city",
+			"name": "Rugeley",
+			"full_name": "Rugeley, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-1.971255,
+							52.739623
+						],
+						[
+							-1.911588,
+							52.739623
+						],
+						[
+							-1.911588,
+							52.773708
+						],
+						[
+							-1.971255,
+							52.773708
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1267410815084146700,
+		"quoted_status_id_str": "1267410815084146689",
+		"quoted_status_permalink": {
+			"url": "https://t.co/W3HMrsDTDu",
+			"expanded": "https://twitter.com/150leaders/status/1267410815084146689",
+			"display": "twitter.com/150leaders/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 01 11:01:00 +0000 2020",
+			"id": 1267410815084146700,
+			"id_str": "1267410815084146689",
+			"full_text": "**Just launched** Our brand new publication Allied Health students: leading the way shares the stories of 11 #student #AHPs and their experiences of #leadership. Thank you to the #150Leaders featured and @NHS_HealthEdEng\nfor your support. \n\nCheck it out 👉 https://t.co/x3vk3HytIN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				279
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "student",
+						"indices": [
+							109,
+							117
+						]
+					},
+					{
+						"text": "AHPs",
+						"indices": [
+							118,
+							123
+						]
+					},
+					{
+						"text": "leadership",
+						"indices": [
+							149,
+							160
+						]
+					},
+					{
+						"text": "150Leaders",
+						"indices": [
+							179,
+							190
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "NHS_HealthEdEng",
+						"name": "NHS HEE #StayHomeSaveLives",
+						"id": 637008966,
+						"id_str": "637008966",
+						"indices": [
+							204,
+							220
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/x3vk3HytIN",
+						"expanded_url": "https://bit.ly/3cnGdDM",
+						"display_url": "bit.ly/3cnGdDM",
+						"indices": [
+							256,
+							279
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1139187039151280100,
+				"id_str": "1139187039151280128",
+				"name": "#150Leaders",
+				"screen_name": "150Leaders",
+				"location": "United Kingdom",
+				"description": "@councilofdeans Student Leadership Programme | co-funded by @BurdettTrust | designed for student nurses, midwives & AHPs across the UK | co-created by students",
+				"url": "https://t.co/AxAGczzJZw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/AxAGczzJZw",
+								"expanded_url": "http://www.councilofdeans.org.uk/studentleadership",
+								"display_url": "councilofdeans.org.uk/studentleaders…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1167,
+				"friends_count": 134,
+				"listed_count": 3,
+				"created_at": "Thu Jun 13 15:05:29 +0000 2019",
+				"favourites_count": 1321,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1345,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1139187039151280128/1583504228",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 16,
+			"favorite_count": 33,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "✨Leading The Way ✨\n⬇️Check out these brilliant #AHP leaders ⬇️\n\nGreat to see @PuppyFlanagan @nhumilton @Vicky_553 have case studies 👍 #150Leaders https://t.co/W3HMrsDTDu"
+	}
+]
