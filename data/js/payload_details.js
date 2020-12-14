987f06e75a1b34bac922b74d3be4aa03197456fc
dwreflections <50467496+dwreflections@users.noreply.github.com>
2020-12-14 01:17:32 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6446,
-	"created_at": "Sun Dec 13 01:17:30 +0000 2020",
+	"tweets": 6447,
+	"created_at": "Mon Dec 14 01:17:30 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sun Dec 13 10:12:47 +0000 2020",
+		"id": 1338064315740459000,
+		"id_str": "1338064315740459008",
+		"full_text": "@Sarahbankssaz3 @davidwalliams Easy mistake to make! 😂👍",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Sarahbankssaz3",
+					"name": "Sarah Egley",
+					"id": 915172193427914800,
+					"id_str": "915172193427914752",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "davidwalliams",
+					"name": "David Walliams HQ",
+					"id": 17441447,
+					"id_str": "17441447",
+					"indices": [
+						16,
+						30
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1338053690624708600,
+		"in_reply_to_status_id_str": "1338053690624708608",
+		"in_reply_to_user_id": 915172193427914800,
+		"in_reply_to_user_id_str": "915172193427914752",
+		"in_reply_to_screen_name": "Sarahbankssaz3",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🎄🎅🏻",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Making a difference✨Physio ✨Chair - Armed Forces Network @UHDBTrust ✨@ccsn_uk Trustee ✨@thecsp Professional Committee ✨@HLA_Int Scholar ✨#150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2554,
+			"friends_count": 3988,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20340,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6766,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Sarahbankssaz3 @davidwalliams Easy mistake to make! 😂👍"
+	},
+	{
 		"created_at": "Sat Dec 12 18:07:45 +0000 2020",
 		"id": 1337821456500224000,
 		"id_str": "1337821456500224002",
@@ -16,7 +119,7 @@ Grailbird.data.tweets_2020_12 =
 			"user_mentions": [
 				{
 					"screen_name": "JghPhysio",
-					"name": "Jack Gladstone Holroyde 🏳️‍🌈🏳️‍⚧️✊🏽",
+					"name": "Jack Gladstone Holroyde 🏳️🌈🏳️⚧️✊🏽",
 					"id": 1046818903337422800,
 					"id_str": "1046818903337422849",
 					"indices": [
