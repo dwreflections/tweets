89b06d12f4cd70ee6f7769ddbf651d176b72ef15
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-01-03 01:17:30 +0000

    Added 5 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,639 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 02 17:36:11 +0000 2021",
+		"id": 1345423659725369300,
+		"id_str": "1345423659725369344",
+		"full_text": "@physio_back @PhysioArmstrong @thecsp Chapeau! 🎩",
+		"truncated": false,
+		"display_text_range": [
+			38,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "physio_back",
+					"name": "ThePhysioBack",
+					"id": 1048100165582839800,
+					"id_str": "1048100165582839808",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "PhysioArmstrong",
+					"name": "James Armstrong",
+					"id": 872165022704840700,
+					"id_str": "872165022704840705",
+					"indices": [
+						13,
+						29
+					]
+				},
+				{
+					"screen_name": "thecsp",
+					"name": "Chartered Society of Physiotherapy",
+					"id": 138098880,
+					"id_str": "138098880",
+					"indices": [
+						30,
+						37
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1345420920895242200,
+		"in_reply_to_status_id_str": "1345420920895242240",
+		"in_reply_to_user_id": 1048100165582839800,
+		"in_reply_to_user_id_str": "1048100165582839808",
+		"in_reply_to_screen_name": "physio_back",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🌟",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio I Chair - Armed Forces Network @UHDBTrust I @ccsn_uk Trustee I @thecsp Professional Committee I @HLA_Int Scholar | #150Leaders #Leadership #Innovation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2564,
+			"friends_count": 4006,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20550,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6815,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "fr",
+		"text": "@physio_back @PhysioArmstrong @thecsp Chapeau! 🎩"
+	},
+	{
+		"created_at": "Sat Jan 02 17:30:05 +0000 2021",
+		"id": 1345422123595141000,
+		"id_str": "1345422123595141121",
+		"full_text": "@LivingWellPain @Connect2Physio I hope to return to Louis book this year. I think they are essential reading for UG courses.... \n\nCan we campaign for the books to be narrated for @audibleuk / other platforms ? Maybe will be picked up wider than “physio”... @dibbygibby thoughts?",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LivingWellPain",
+					"name": "Tina",
+					"id": 1008439629912166400,
+					"id_str": "1008439629912166401",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "Connect2Physio",
+					"name": "Connect2Physio",
+					"id": 1266685426523406300,
+					"id_str": "1266685426523406337",
+					"indices": [
+						16,
+						31
+					]
+				},
+				{
+					"screen_name": "audibleuk",
+					"name": "Audible UK",
+					"id": 21996576,
+					"id_str": "21996576",
+					"indices": [
+						179,
+						189
+					]
+				},
+				{
+					"screen_name": "dibbygibby",
+					"name": "Prof Mick Thacker",
+					"id": 105141747,
+					"id_str": "105141747",
+					"indices": [
+						257,
+						268
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1345386418613448700,
+		"in_reply_to_status_id_str": "1345386418613448704",
+		"in_reply_to_user_id": 1008439629912166400,
+		"in_reply_to_user_id_str": "1008439629912166401",
+		"in_reply_to_screen_name": "LivingWellPain",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🌟",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio I Chair - Armed Forces Network @UHDBTrust I @ccsn_uk Trustee I @thecsp Professional Committee I @HLA_Int Scholar | #150Leaders #Leadership #Innovation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2564,
+			"friends_count": 4006,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20550,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6815,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LivingWellPain @Connect2Physio I hope to return to Louis book this year. I think they are essential reading for UG courses.... \n\nCan we campaign for the books to be narrated for @audibleuk / other platforms ? Maybe will be picked up wider than “physio”... @dibbygibby thoughts?"
+	},
+	{
+		"created_at": "Sat Jan 02 17:11:19 +0000 2021",
+		"id": 1345389040745508900,
+		"id_str": "1345417401878327300",
+		"full_text": "If you are wondering who to follow &amp; engage on twitter, take a look at The HLA Community:\n\nhttps://t.co/2TSn2Su47F \n\nThese are all our HLA Scholars and Community active on Twitter. There are a huge variety of individuals with a huge variety of opinions\n\n#medtwitter #NurseTwitter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "medtwitter",
+					"indices": [
+						258,
+						269
+					]
+				},
+				{
+					"text": "NurseTwitter",
+					"indices": [
+						270,
+						283
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2TSn2Su47F",
+					"expanded_url": "https://twitter.com/i/lists/1282778389951270912",
+					"display_url": "twitter.com/i/lists/128277…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 809803519846993900,
+			"id_str": "809803519846993921",
+			"name": "The HLA",
+			"screen_name": "HLA_int",
+			"location": "",
+			"description": "The Healthcare Leadership Academy: \n🌱Nurturing leadership🌱\n💭Fostering frontline innovation💭\n⚡Empowering and valuing the next generation⚡\n\nConf: 21&22 Nov",
+			"url": "https://t.co/84YgqZTCrl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/84YgqZTCrl",
+							"expanded_url": "http://thehealthcareleadership.academy",
+							"display_url": "thehealthcareleadership.academy",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3766,
+			"friends_count": 0,
+			"listed_count": 19,
+			"created_at": "Fri Dec 16 16:52:59 +0000 2016",
+			"favourites_count": 4783,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9076,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/810983604406915073/2jjtK_O1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/810983604406915073/2jjtK_O1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/809803519846993921/1590615561",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you are wondering who to follow &amp; engage on twitter, take a look at The HLA Community:\n\nhttps://t.co/2TSn2Su47F \n\nThese are all our HLA Scholars and Community active on Twitter. There are a huge variety of individuals with a huge variety of opinions\n\n#medtwitter #NurseTwitter"
+	},
+	{
+		"created_at": "Sat Jan 02 14:56:11 +0000 2021",
+		"id": 1345383392335036400,
+		"id_str": "1345383392335036419",
+		"full_text": "@DrBetofMDPhD Wow 😍",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DrBetofMDPhD",
+					"name": "Allison Betof Warner, MD, PhD",
+					"id": 721788669749698600,
+					"id_str": "721788669749698560",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1345069537557930000,
+		"in_reply_to_status_id_str": "1345069537557929986",
+		"in_reply_to_user_id": 721788669749698600,
+		"in_reply_to_user_id_str": "721788669749698560",
+		"in_reply_to_screen_name": "DrBetofMDPhD",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🌟",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio I Chair - Armed Forces Network @UHDBTrust I @ccsn_uk Trustee I @thecsp Professional Committee I @HLA_Int Scholar | #150Leaders #Leadership #Innovation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2564,
+			"friends_count": 4006,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20550,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6815,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@DrBetofMDPhD Wow 😍"
+	},
+	{
+		"created_at": "Sat Jan 02 14:45:27 +0000 2021",
+		"id": 1345380693480779800,
+		"id_str": "1345380693480779776",
+		"full_text": "@theEndurancePT @UKRunChat @UkTriChat @IronmanAthletes @trihquk @OutlawTriathlon @RunWales @FastRunning @letsrundotcom @lawro28",
+		"truncated": false,
+		"display_text_range": [
+			119,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "theEndurancePT",
+					"name": "The Endurance Physio",
+					"id": 3319174982,
+					"id_str": "3319174982",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "UKRunChat",
+					"name": "UKRunChat™️",
+					"id": 1602985406,
+					"id_str": "1602985406",
+					"indices": [
+						16,
+						26
+					]
+				},
+				{
+					"screen_name": "UkTriChat",
+					"name": "UKTriChat™️",
+					"id": 2273404677,
+					"id_str": "2273404677",
+					"indices": [
+						27,
+						37
+					]
+				},
+				{
+					"screen_name": "IronmanAthletes",
+					"name": "Ironman Athletes",
+					"id": 39362679,
+					"id_str": "39362679",
+					"indices": [
+						38,
+						54
+					]
+				},
+				{
+					"screen_name": "trihquk",
+					"name": "Tri HQ",
+					"id": 948967390527852500,
+					"id_str": "948967390527852544",
+					"indices": [
+						55,
+						63
+					]
+				},
+				{
+					"screen_name": "OutlawTriathlon",
+					"name": "Outlaw Triathlon",
+					"id": 73091562,
+					"id_str": "73091562",
+					"indices": [
+						64,
+						80
+					]
+				},
+				{
+					"screen_name": "RunWales",
+					"name": "I Run Wales",
+					"id": 3021558346,
+					"id_str": "3021558346",
+					"indices": [
+						81,
+						90
+					]
+				},
+				{
+					"screen_name": "FastRunning",
+					"name": "Fast Running",
+					"id": 2840612943,
+					"id_str": "2840612943",
+					"indices": [
+						91,
+						103
+					]
+				},
+				{
+					"screen_name": "letsrundotcom",
+					"name": "LetsRun.com",
+					"id": 28848474,
+					"id_str": "28848474",
+					"indices": [
+						104,
+						118
+					]
+				},
+				{
+					"screen_name": "lawro28",
+					"name": "Lawro",
+					"id": 146907370,
+					"id_str": "146907370",
+					"indices": [
+						119,
+						127
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1345380220073861000,
+		"in_reply_to_status_id_str": "1345380220073861122",
+		"in_reply_to_user_id": 3319174982,
+		"in_reply_to_user_id_str": "3319174982",
+		"in_reply_to_screen_name": "theEndurancePT",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🌟",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio I Chair - Armed Forces Network @UHDBTrust I @ccsn_uk Trustee I @thecsp Professional Committee I @HLA_Int Scholar | #150Leaders #Leadership #Innovation",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2564,
+			"friends_count": 4006,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20550,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6815,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@theEndurancePT @UKRunChat @UkTriChat @IronmanAthletes @trihquk @OutlawTriathlon @RunWales @FastRunning @letsrundotcom @lawro28"
+	},
+	{
 		"created_at": "Fri Jan 01 15:50:26 +0000 2021",
 		"id": 1345034657721684000,
 		"id_str": "1345034657721683973",
