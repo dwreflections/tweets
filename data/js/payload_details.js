6054edbeca8cbe174e8aa7f033d9b34170568d94
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-02-02 01:17:31 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6544,
-	"created_at": "Mon Feb 01 01:17:31 +0000 2021",
+	"tweets": 6545,
+	"created_at": "Tue Feb 02 01:17:30 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,145 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Mon Feb 01 19:06:48 +0000 2021",
+		"id": 1356272849250562000,
+		"id_str": "1356318096848531461",
+		"full_text": "We're really looking forward to our online session on being a cultural ambassador for change with @DonnellyHelene tonight at 8pm. #FTSU @NatGuardianFTSU #150Leaders\n\n👇 for any last minute joiners \nhttps://t.co/TlWB34BNBT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FTSU",
+					"indices": [
+						130,
+						135
+					]
+				},
+				{
+					"text": "150Leaders",
+					"indices": [
+						153,
+						164
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DonnellyHelene",
+					"name": "Helene E Donnelly",
+					"id": 1451336624,
+					"id_str": "1451336624",
+					"indices": [
+						98,
+						113
+					]
+				},
+				{
+					"screen_name": "NatGuardianFTSU",
+					"name": "National Guardian's Office 💙",
+					"id": 740855642223697900,
+					"id_str": "740855642223697921",
+					"indices": [
+						136,
+						152
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TlWB34BNBT",
+					"expanded_url": "https://bit.ly/2YuzbJ4",
+					"display_url": "bit.ly/2YuzbJ4",
+					"indices": [
+						197,
+						220
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1139187039151280100,
+			"id_str": "1139187039151280128",
+			"name": "Student Leadership Programme #150Leaders",
+			"screen_name": "150Leaders",
+			"location": "United Kingdom",
+			"description": "@councilofdeans Student Leadership Programme | co-funded by @BurdettTrust | designed for student nurses, midwives & AHPs across the UK | co-created by students",
+			"url": "https://t.co/AxAGczzJZw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AxAGczzJZw",
+							"expanded_url": "http://www.councilofdeans.org.uk/studentleadership",
+							"display_url": "councilofdeans.org.uk/studentleaders…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1770,
+			"friends_count": 228,
+			"listed_count": 6,
+			"created_at": "Thu Jun 13 15:05:29 +0000 2019",
+			"favourites_count": 2673,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2397,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1139187039151280128/1583504228",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 11,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're really looking forward to our online session on being a cultural ambassador for change with @DonnellyHelene tonight at 8pm. #FTSU @NatGuardianFTSU #150Leaders\n\n👇 for any last minute joiners \nhttps://t.co/TlWB34BNBT"
+	}
+]
