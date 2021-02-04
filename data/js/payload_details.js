3f80ae5c64ac671fb3586a9a612179b0b0f8ba69
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-02-04 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6546,
-	"created_at": "Wed Feb 03 01:17:29 +0000 2021",
+	"tweets": 6547,
+	"created_at": "Thu Feb 04 01:17:30 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,342 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Wed Feb 03 16:55:26 +0000 2021",
+		"id": 1357009814551953400,
+		"id_str": "1357009814551953410",
+		"full_text": "“Because we know you like a lie in.. “\n\nPhysio students, new grads &amp; MSK’ers get involved. @PMFirstSteps @bell_bentley ☕️ https://t.co/HOsGeByFz0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PMFirstSteps",
+					"name": "Physio Matters First Steps",
+					"id": 840861722286137300,
+					"id_str": "840861722286137349",
+					"indices": [
+						95,
+						108
+					]
+				},
+				{
+					"screen_name": "bell_bentley",
+					"name": "Amelia",
+					"id": 469375429,
+					"id_str": "469375429",
+					"indices": [
+						109,
+						122
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HOsGeByFz0",
+					"expanded_url": "https://twitter.com/pmfirststeps/status/1356992036285665280",
+					"display_url": "twitter.com/pmfirststeps/s…",
+					"indices": [
+						126,
+						149
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Midlands",
+			"description": "Physio interested in #leadership & #innovation IChair -Armed Forces Network @UHDBTrust |Governor @suaupdates I @thecsp Professional Committee I @HLA_Int Scholar",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2620,
+			"friends_count": 4065,
+			"listed_count": 17,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 20916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6890,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1356992036285665300,
+		"quoted_status_id_str": "1356992036285665280",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HOsGeByFz0",
+			"expanded": "https://twitter.com/pmfirststeps/status/1356992036285665280",
+			"display": "twitter.com/pmfirststeps/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Feb 03 15:44:47 +0000 2021",
+			"id": 1356992036285665300,
+			"id_str": "1356992036285665280",
+			"full_text": "☕☕☕ drop-in for a chat, hosted by @PMFirstSteps 13:00-15:00 Saturday - DM for link https://t.co/FuhZ8ih1eM",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				82
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "PMFirstSteps",
+						"name": "Physio Matters First Steps",
+						"id": 840861722286137300,
+						"id_str": "840861722286137349",
+						"indices": [
+							34,
+							47
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1356991918673191000,
+						"id_str": "1356991918673190912",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1356991918673190912/pu/img/TGBA2oD0Uyrn_-E_.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1356991918673190912/pu/img/TGBA2oD0Uyrn_-E_.jpg",
+						"url": "https://t.co/FuhZ8ih1eM",
+						"display_url": "pic.twitter.com/FuhZ8ih1eM",
+						"expanded_url": "https://twitter.com/PMFirstSteps/status/1356992036285665280/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 604,
+								"h": 1280,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 321,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 566,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1356991918673191000,
+						"id_str": "1356991918673190912",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1356991918673190912/pu/img/TGBA2oD0Uyrn_-E_.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1356991918673190912/pu/img/TGBA2oD0Uyrn_-E_.jpg",
+						"url": "https://t.co/FuhZ8ih1eM",
+						"display_url": "pic.twitter.com/FuhZ8ih1eM",
+						"expanded_url": "https://twitter.com/PMFirstSteps/status/1356992036285665280/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 604,
+								"h": 1280,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 321,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 566,
+								"h": 1200,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								151,
+								320
+							],
+							"duration_millis": 44534,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1356991918673190912/pu/pl/RuDEvWqjg8avkIGe.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1356991918673190912/pu/vid/360x762/VuESlzZBkVP9Qhce.mp4?tag=10"
+								},
+								{
+									"bitrate": 632000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1356991918673190912/pu/vid/320x678/Yr8Y7qOAgU2ho-Hu.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1356991918673190912/pu/vid/604x1280/0nczJ3iG8TFagSjp.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 840861722286137300,
+				"id_str": "840861722286137349",
+				"name": "Physio Matters First Steps",
+				"screen_name": "PMFirstSteps",
+				"location": "",
+				"description": "Run by/for therapy students & new grads.\nExplore those “stupid” questions, build connections & share experiences.\nSupported by @TPMPodcast\nNewsletter ⬇",
+				"url": "https://t.co/tq9vjQsSVt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/tq9vjQsSVt",
+								"expanded_url": "https://mailchi.mp/e343b3da2651/firs-steps",
+								"display_url": "mailchi.mp/e343b3da2651/f…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1598,
+				"friends_count": 165,
+				"listed_count": 8,
+				"created_at": "Sun Mar 12 09:47:11 +0000 2017",
+				"favourites_count": 637,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 757,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1273929840358555652/mP6SWVq7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1273929840358555652/mP6SWVq7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/840861722286137349/1592563518",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Because we know you like a lie in.. “\n\nPhysio students, new grads &amp; MSK’ers get involved. @PMFirstSteps @bell_bentley ☕️ https://t.co/HOsGeByFz0"
+	},
+	{
 		"created_at": "Tue Feb 02 21:22:30 +0000 2021",
 		"id": 1356714636284997600,
 		"id_str": "1356714636284997635",
