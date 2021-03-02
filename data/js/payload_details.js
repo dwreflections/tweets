6518138bfd2f9dec71e0c4883fbbf0d1f72dbbc4
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-03-02 01:17:31 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6653,
-	"created_at": "Mon Mar 01 01:17:30 +0000 2021",
+	"tweets": 6654,
+	"created_at": "Tue Mar 02 01:17:30 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,279 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Mon Mar 01 21:44:40 +0000 2021",
+		"id": 1366504128227254300,
+		"id_str": "1366504689060241414",
+		"full_text": "#AHPStudents 👀👀\n\nSignificant early #AHPLeadership opportunity 👉 apply to become member of next #150Leaders cohort\n\n@WeAHPs @councilofdeans https://t.co/2RJqJkLOR0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AHPStudents",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"text": "AHPLeadership",
+					"indices": [
+						35,
+						49
+					]
+				},
+				{
+					"text": "150Leaders",
+					"indices": [
+						95,
+						106
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WeAHPs",
+					"name": "WeAHPs 💙",
+					"id": 2364573882,
+					"id_str": "2364573882",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"screen_name": "councilofdeans",
+					"name": "Council of Deans of Health",
+					"id": 278442312,
+					"id_str": "278442312",
+					"indices": [
+						123,
+						138
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2RJqJkLOR0",
+					"expanded_url": "https://twitter.com/150leaders/status/1364915799987793924",
+					"display_url": "twitter.com/150leaders/sta…",
+					"indices": [
+						139,
+						162
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1596992540,
+			"id_str": "1596992540",
+			"name": "Suzanne Rastrick",
+			"screen_name": "SuzanneRastrick",
+			"location": "England",
+			"description": "Chief Allied Health Professions Officer (CAHPO) for England @NHSEngland #AHPsintoAction #DigitalAHPs #AHPLeadership | NED in housing sector",
+			"url": "https://t.co/D4UEMbVmju",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/D4UEMbVmju",
+							"expanded_url": "https://www.england.nhs.uk/ahp/",
+							"display_url": "england.nhs.uk/ahp/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16554,
+			"friends_count": 691,
+			"listed_count": 97,
+			"created_at": "Mon Jul 15 23:19:04 +0000 2013",
+			"favourites_count": 69523,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25982,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1184579419127668742/6ULPHKXT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1184579419127668742/6ULPHKXT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1596992540/1584777837",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1364915799987794000,
+		"quoted_status_id_str": "1364915799987793924",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2RJqJkLOR0",
+			"expanded": "https://twitter.com/150leaders/status/1364915799987793924",
+			"display": "twitter.com/150leaders/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Feb 25 12:31:00 +0000 2021",
+			"id": 1364915799987794000,
+			"id_str": "1364915799987793924",
+			"full_text": "📢 Applications for the new #150Leaders cohort will open on 1 April and will close on 25 April 2021. All the information is on our website.\n\nRegister for our webinar for applicants on 30 March at 8pm. Ask all your questions and meet some #150Leaders: https://t.co/PXDE916DkW",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				273
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "150Leaders",
+						"indices": [
+							27,
+							38
+						]
+					},
+					{
+						"text": "150Leaders",
+						"indices": [
+							237,
+							248
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/PXDE916DkW",
+						"expanded_url": "https://bit.ly/37ty1lM",
+						"display_url": "bit.ly/37ty1lM",
+						"indices": [
+							250,
+							273
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1139187039151280100,
+				"id_str": "1139187039151280128",
+				"name": "Student Leadership Programme #150Leaders",
+				"screen_name": "150Leaders",
+				"location": "United Kingdom",
+				"description": "@councilofdeans Student Leadership Programme | co-funded by @BurdettTrust | designed for student nurses, midwives & AHPs across the UK | co-created by students",
+				"url": "https://t.co/AxAGczzJZw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/AxAGczzJZw",
+								"expanded_url": "http://www.councilofdeans.org.uk/studentleadership",
+								"display_url": "councilofdeans.org.uk/studentleaders…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1922,
+				"friends_count": 241,
+				"listed_count": 6,
+				"created_at": "Thu Jun 13 15:05:29 +0000 2019",
+				"favourites_count": 2903,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2524,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1141695642672279552/rf-wEzoI_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1139187039151280128/1583504228",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 100,
+			"favorite_count": 138,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 11,
+		"favorite_count": 13,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#AHPStudents 👀👀\n\nSignificant early #AHPLeadership opportunity 👉 apply to become member of next #150Leaders cohort\n\n@WeAHPs @councilofdeans https://t.co/2RJqJkLOR0"
+	}
+]
