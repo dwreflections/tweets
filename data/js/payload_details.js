79c3ba16218408abb2ad92ef8060993859d5089e
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-03-07 01:17:29 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6666,
-	"created_at": "Sat Mar 06 01:17:29 +0000 2021",
+	"tweets": 6668,
+	"created_at": "Sun Mar 07 01:17:29 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Sat Mar 06 10:59:52 +0000 2021",
+		"id": 1368154355317215200,
+		"id_str": "1368154355317215234",
+		"full_text": "@BrianWebster18 @nmcnews @WeStudentNurse @StNurseProject You need to speak with @exerciseworks",
+		"truncated": false,
+		"display_text_range": [
+			57,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BrianWebster18",
+					"name": "Brian Webster",
+					"id": 988666375,
+					"id_str": "988666375",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "nmcnews",
+					"name": "Nursing and Midwifery Council",
+					"id": 61489404,
+					"id_str": "61489404",
+					"indices": [
+						16,
+						24
+					]
+				},
+				{
+					"screen_name": "WeStudentNurse",
+					"name": "WeStudentNurses 💙",
+					"id": 939235274554560500,
+					"id_str": "939235274554560512",
+					"indices": [
+						25,
+						40
+					]
+				},
+				{
+					"screen_name": "StNurseProject",
+					"name": "The Student Nurse Project",
+					"id": 943543366381375500,
+					"id_str": "943543366381375488",
+					"indices": [
+						41,
+						56
+					]
+				},
+				{
+					"screen_name": "exerciseworks",
+					"name": "Exercise Works!",
+					"id": 256921454,
+					"id_str": "256921454",
+					"indices": [
+						80,
+						94
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1368153746975375400,
+		"in_reply_to_status_id_str": "1368153746975375366",
+		"in_reply_to_user_id": 988666375,
+		"in_reply_to_user_id_str": "988666375",
+		"in_reply_to_screen_name": "BrianWebster18",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Midlands",
+			"description": "Physiotherapy, leadership etc.📚👥",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2697,
+			"friends_count": 4175,
+			"listed_count": 18,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 21503,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7029,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@BrianWebster18 @nmcnews @WeStudentNurse @StNurseProject You need to speak with @exerciseworks"
+	},
+	{
+		"created_at": "Sat Mar 06 10:55:24 +0000 2021",
+		"id": 1368153232862834700,
+		"id_str": "1368153232862834689",
+		"full_text": "@DrDavidWarriner I’ve lost weight and lots of muscle mass. A few months of being very sedentary and a loss of appetite with long COVID... gradually increasing the phys back up now! 💪",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			182
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DrDavidWarriner",
+					"name": "David Warriner",
+					"id": 954526350,
+					"id_str": "954526350",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1367833403035357200,
+		"in_reply_to_status_id_str": "1367833403035357184",
+		"in_reply_to_user_id": 954526350,
+		"in_reply_to_user_id_str": "954526350",
+		"in_reply_to_screen_name": "DrDavidWarriner",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Midlands",
+			"description": "Physiotherapy, leadership etc.📚👥",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2697,
+			"friends_count": 4175,
+			"listed_count": 18,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 21503,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7029,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@DrDavidWarriner I’ve lost weight and lots of muscle mass. A few months of being very sedentary and a loss of appetite with long COVID... gradually increasing the phys back up now! 💪"
+	},
+	{
 		"created_at": "Fri Mar 05 21:52:08 +0000 2021",
 		"id": 1367956115712000000,
 		"id_str": "1367956115712000011",
