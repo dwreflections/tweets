463b66a7c2305f5ddcccd327a526f12c407a407b
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-04-02 02:17:31 +0100

    Added 1 tweets

---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,106 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Thu Apr 01 19:07:05 +0000 2021",
+		"id": 1377699055317479400,
+		"id_str": "1377699055317479430",
+		"full_text": "@HMSSherwood Congratulations @shellyrowlands 👏",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HMSSherwood",
+					"name": "HMS Sherwood",
+					"id": 2446041936,
+					"id_str": "2446041936",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "shellyrowlands",
+					"name": "Chelle R",
+					"id": 83087399,
+					"id_str": "83087399",
+					"indices": [
+						29,
+						44
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1377647559871713300,
+		"in_reply_to_status_id_str": "1377647559871713285",
+		"in_reply_to_user_id": 2446041936,
+		"in_reply_to_user_id_str": "2446041936",
+		"in_reply_to_screen_name": "HMSSherwood",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio with interests in innovation & leadership ▪️ Armed Forces Champion ▪️ @hla_int Scholar ▪️ #150Leaders ▪️ CSP Professional Committee ▪️ School Governor",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2736,
+			"friends_count": 4224,
+			"listed_count": 18,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 21760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7101,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@HMSSherwood Congratulations @shellyrowlands 👏"
+	}
+]
