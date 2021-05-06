c1f77adbba5f3181d3233b2fffd01c2e44b763e6
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-05-06 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6773,
-	"created_at": "Mon May 03 01:17:29 +0000 2021",
+	"tweets": 6774,
+	"created_at": "Thu May 06 01:17:29 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Wed May 05 11:55:36 +0000 2021",
+		"id": 1389907150902907000,
+		"id_str": "1389911655107203076",
+		"full_text": "What will the UK look like in 2030? And how will this impact the Armed Forces Community and the organisations that support them? \n\n🗓 Join us at 10am on 27th May for the online launch of our future trends report #LiftingOurSights\n\nSign up here 👇 \nhttps://t.co/cTASq15bLH https://t.co/yIRSiuBcwp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LiftingOurSights",
+					"indices": [
+						211,
+						228
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cTASq15bLH",
+					"expanded_url": "https://www.eventbrite.co.uk/e/lifting-our-sights-beyond-2030-tickets-151663851663?aff=ebdsoporgprofile",
+					"display_url": "eventbrite.co.uk/e/lifting-our-…",
+					"indices": [
+						246,
+						269
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1389904422617108500,
+					"id_str": "1389904422617108483",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/E0nutVXWUAMSIau.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0nutVXWUAMSIau.jpg",
+					"url": "https://t.co/yIRSiuBcwp",
+					"display_url": "pic.twitter.com/yIRSiuBcwp",
+					"expanded_url": "https://twitter.com/FiMTrust/status/1389907150902906882/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1169,
+							"h": 537,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1169,
+							"h": 537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 312,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1389904422617108500,
+					"id_str": "1389904422617108483",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/E0nutVXWUAMSIau.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0nutVXWUAMSIau.jpg",
+					"url": "https://t.co/yIRSiuBcwp",
+					"display_url": "pic.twitter.com/yIRSiuBcwp",
+					"expanded_url": "https://twitter.com/FiMTrust/status/1389907150902906882/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1169,
+							"h": 537,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1169,
+							"h": 537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 312,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2188472936,
+			"id_str": "2188472936",
+			"name": "Forces in Mind Trust",
+			"screen_name": "FiMTrust",
+			"location": "London",
+			"description": "Seeking to improve transition for all UK military personnel and their families.",
+			"url": "http://t.co/DnFsZdT4fL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/DnFsZdT4fL",
+							"expanded_url": "http://www.fim-trust.org/",
+							"display_url": "fim-trust.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4724,
+			"friends_count": 926,
+			"listed_count": 41,
+			"created_at": "Mon Nov 11 14:24:51 +0000 2013",
+			"favourites_count": 2986,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4008,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925389981035745282/OwqnXNP1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925389981035745282/OwqnXNP1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2188472936/1561374339",
+			"profile_link_color": "3A0E4D",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 7,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What will the UK look like in 2030? And how will this impact the Armed Forces Community and the organisations that support them? \n\n🗓 Join us at 10am on 27th May for the online launch of our future trends report #LiftingOurSights\n\nSign up here 👇 \nhttps://t.co/cTASq15bLH https://t.co/yIRSiuBcwp"
+	},
+	{
 		"created_at": "Sun May 02 21:49:07 +0000 2021",
 		"id": 1388973856669503500,
 		"id_str": "1388973856669503488",
