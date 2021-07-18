ddd95e385ad082c7bbea687d4d6a1d11592ace10
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-07-18 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6969,
-	"created_at": "Sat Jul 17 01:17:29 +0000 2021",
+	"tweets": 6970,
+	"created_at": "Sun Jul 18 01:17:29 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sat Jul 17 11:06:50 +0000 2021",
+		"id": 1416339068586188800,
+		"id_str": "1416353697219678208",
+		"full_text": "ISSUE 01 IS PUBLISHED 🎉\n\nRead the full issue including our interviews with @a_c_harvey, @saritalacolocha  &amp; @malamawkin below.\n\nhttps://t.co/trJEmME96q\n\nIf you are on mobile, you can access mobile-friendly versions of each of our articles in this thread...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "a_c_harvey",
+					"name": "Dr Anna Harvey she/her 🌱🌿 (views mine)",
+					"id": 911267446996262900,
+					"id_str": "911267446996262912",
+					"indices": [
+						75,
+						86
+					]
+				},
+				{
+					"screen_name": "saritalacolocha",
+					"name": "sarita",
+					"id": 2878038100,
+					"id_str": "2878038100",
+					"indices": [
+						88,
+						104
+					]
+				},
+				{
+					"screen_name": "malamawkin",
+					"name": "Dr Mala Mawkin",
+					"id": 480740170,
+					"id_str": "480740170",
+					"indices": [
+						112,
+						123
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/trJEmME96q",
+					"expanded_url": "https://issuu.com/medicish/docs/medicish_issue_1",
+					"display_url": "issuu.com/medicish/docs/…",
+					"indices": [
+						132,
+						155
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1300002525463150600,
+			"id_str": "1300002525463150594",
+			"name": "Medicish - issue 01 out now!",
+			"screen_name": "Medicish_",
+			"location": "",
+			"description": "Medicish is a movement showcasing alternative lifestyles and unique career paths within the health professions.  #StillAProfessional\n\nIssue 01 out now!",
+			"url": "https://t.co/ALcHSbyzk1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ALcHSbyzk1",
+							"expanded_url": "https://issuu.com/medicish/docs/medicish_issue_1",
+							"display_url": "issuu.com/medicish/docs/…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Sun Aug 30 09:28:57 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1415958499297738752/HphpyTgC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1415958499297738752/HphpyTgC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1300002525463150594/1626425789",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "ISSUE 01 IS PUBLISHED 🎉\n\nRead the full issue including our interviews with @a_c_harvey, @saritalacolocha  &amp; @malamawkin below.\n\nhttps://t.co/trJEmME96q\n\nIf you are on mobile, you can access mobile-friendly versions of each of our articles in this thread..."
+	},
+	{
 		"created_at": "Fri Jul 16 20:29:03 +0000 2021",
 		"id": 1416132794196107300,
 		"id_str": "1416132794196107264",
