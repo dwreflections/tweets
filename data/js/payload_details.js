29e8e680fa46a6829804af4f7b8db61266b559fc
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-08-02 02:17:31 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6998,
-	"created_at": "Sun Aug 01 01:17:30 +0000 2021",
+	"tweets": 6999,
+	"created_at": "Mon Aug 02 01:17:30 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,385 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Sun Aug 01 18:27:38 +0000 2021",
+		"id": 1421900445077971000,
+		"id_str": "1421900445077970949",
+		"full_text": "Chapeau! 🥇 https://t.co/U3n1q9ToXI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U3n1q9ToXI",
+					"expanded_url": "https://twitter.com/TeamGB/status/1421654527296491529",
+					"display_url": "twitter.com/TeamGB/status/…",
+					"indices": [
+						11,
+						34
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust & @suaupdates | Chair - Armed Forces Network (UHDB) | Professional Committee @thecsp | @HLA_int Scholar | #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2940,
+			"friends_count": 4583,
+			"listed_count": 0,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23218,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7433,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1421654527296491500,
+		"quoted_status_id_str": "1421654527296491529",
+		"quoted_status_permalink": {
+			"url": "https://t.co/U3n1q9ToXI",
+			"expanded": "https://twitter.com/TeamGB/status/1421654527296491529",
+			"display": "twitter.com/TeamGB/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Aug 01 02:10:27 +0000 2021",
+			"id": 1421654527296491500,
+			"id_str": "1421654527296491529",
+			"full_text": "🔥🔥🔥\n\n@chazworther nails the first 360 backflip - a trick never landed before in a women's competition - to claim gold.  \n\n🎥 @BBCSport \nhttps://t.co/j9osc7bMFe",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				158
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "chazworther",
+						"name": "Charlotte Worthington",
+						"id": 872464489,
+						"id_str": "872464489",
+						"indices": [
+							5,
+							17
+						]
+					},
+					{
+						"screen_name": "BBCSport",
+						"name": "BBC Sport",
+						"id": 265902729,
+						"id_str": "265902729",
+						"indices": [
+							124,
+							133
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1421651399650205700,
+						"id_str": "1421651399650205697",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/media/E7q4dU7WUAQZn25.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E7q4dU7WUAQZn25.jpg",
+						"url": "https://t.co/j9osc7bMFe",
+						"display_url": "pic.twitter.com/j9osc7bMFe",
+						"expanded_url": "https://twitter.com/BBCSport/status/1421651516889444354/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1421651516889444400,
+						"source_status_id_str": "1421651516889444354",
+						"source_user_id": 265902729,
+						"source_user_id_str": "265902729"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1421651399650205700,
+						"id_str": "1421651399650205697",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/media/E7q4dU7WUAQZn25.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E7q4dU7WUAQZn25.jpg",
+						"url": "https://t.co/j9osc7bMFe",
+						"display_url": "pic.twitter.com/j9osc7bMFe",
+						"expanded_url": "https://twitter.com/BBCSport/status/1421651516889444354/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1421651516889444400,
+						"source_status_id_str": "1421651516889444354",
+						"source_user_id": 265902729,
+						"source_user_id_str": "265902729",
+						"additional_media_info": {
+							"title": "Women's BMX freestyle final",
+							"description": "Watch & follow on the BBC",
+							"embeddable": false,
+							"monetizable": false,
+							"source_user": {
+								"id": 265902729,
+								"id_str": "265902729",
+								"name": "BBC Sport",
+								"screen_name": "BBCSport",
+								"location": "MediaCityUK, Salford",
+								"description": "Official https://t.co/pgyB0HHtMO account. Also follow @bbcmotd and @bbctms.",
+								"url": "https://t.co/XsBH2P4slh",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "https://t.co/XsBH2P4slh",
+												"expanded_url": "http://www.bbc.co.uk/sport",
+												"display_url": "bbc.co.uk/sport",
+												"indices": [
+													0,
+													23
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": [
+											{
+												"url": "https://t.co/pgyB0HHtMO",
+												"expanded_url": "http://bbc.co.uk/sport",
+												"display_url": "bbc.co.uk/sport",
+												"indices": [
+													9,
+													32
+												]
+											}
+										]
+									}
+								},
+								"protected": false,
+								"followers_count": 9337290,
+								"friends_count": 338,
+								"listed_count": 21222,
+								"created_at": "Mon Mar 14 09:44:40 +0000 2011",
+								"favourites_count": 2240,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": false,
+								"verified": true,
+								"statuses_count": 513186,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": true,
+								"profile_background_color": "C0DEED",
+								"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+								"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/968086146180886528/P_nvzGw__normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/968086146180886528/P_nvzGw__normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/265902729/1626418435",
+								"profile_link_color": "FFD230",
+								"profile_sidebar_border_color": "C0DEED",
+								"profile_sidebar_fill_color": "DDEEF6",
+								"profile_text_color": "333333",
+								"profile_use_background_image": true,
+								"has_extended_profile": false,
+								"default_profile": false,
+								"default_profile_image": false,
+								"following": true,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none",
+								"withheld_in_countries": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 27660239,
+				"id_str": "27660239",
+				"name": "Team GB",
+				"screen_name": "TeamGB",
+				"location": "London",
+				"description": "Team GB is the Great Britain and Northern Ireland Olympic Team run by the British Olympic Association",
+				"url": "https://t.co/KoBWQLaIxU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/KoBWQLaIxU",
+								"expanded_url": "http://www.TeamGB.com/",
+								"display_url": "TeamGB.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 914143,
+				"friends_count": 1398,
+				"listed_count": 3269,
+				"created_at": "Mon Mar 30 15:34:21 +0000 2009",
+				"favourites_count": 9141,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 44717,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "161821",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1336950029244903424/F3db-gR2_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1336950029244903424/F3db-gR2_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27660239/1627791498",
+				"profile_link_color": "0C7CC7",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "D3DEEB",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1133,
+			"favorite_count": 8612,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Chapeau! 🥇 https://t.co/U3n1q9ToXI"
+	}
+]
