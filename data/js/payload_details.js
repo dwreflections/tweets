54a6eedbc829f570a595894637da1cf8d997327b
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-08-29 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7031,
-	"created_at": "Sat Aug 28 01:17:29 +0000 2021",
+	"tweets": 7032,
+	"created_at": "Sun Aug 29 01:17:29 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Sat Aug 28 19:57:05 +0000 2021",
+		"id": 1431707427632783400,
+		"id_str": "1431707427632783369",
+		"full_text": "@MaheySangeeta @DanylJoe @gavinUHDB @harinder_d @SharonExtance @alisonthorp @mwansamulenga03 @icelsuarez1 @DrSarahToddUHDB “Challenging the status quo takes commitment, courage, imagination, and, above all, dedication to learning.” - Marshall Ganz ✨\n\nThank you for being the powerhouse @MaheySangeeta",
+		"truncated": false,
+		"display_text_range": [
+			123,
+			300
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MaheySangeeta",
+					"name": "Sangeeta Mahey",
+					"id": 1144244650972258300,
+					"id_str": "1144244650972258305",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "DanylJoe",
+					"name": "DanylJoe Walton-Ashmore (He/Him)",
+					"id": 243799802,
+					"id_str": "243799802",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"screen_name": "gavinUHDB",
+					"name": "Gavin Boyle",
+					"id": 1364206064,
+					"id_str": "1364206064",
+					"indices": [
+						25,
+						35
+					]
+				},
+				{
+					"screen_name": "harinder_d",
+					"name": "Harinder D",
+					"id": 1379751296,
+					"id_str": "1379751296",
+					"indices": [
+						36,
+						47
+					]
+				},
+				{
+					"screen_name": "SharonExtance",
+					"name": "Sharon Extance💙 pronouns She/Her",
+					"id": 994647938,
+					"id_str": "994647938",
+					"indices": [
+						48,
+						62
+					]
+				},
+				{
+					"screen_name": "alisonthorp",
+					"name": "Alison Thorp 💙😇",
+					"id": 2790636631,
+					"id_str": "2790636631",
+					"indices": [
+						63,
+						75
+					]
+				},
+				{
+					"screen_name": "mwansamulenga03",
+					"name": "Mwansa Mulenga",
+					"id": 1380595874997358600,
+					"id_str": "1380595874997358603",
+					"indices": [
+						76,
+						92
+					]
+				},
+				{
+					"screen_name": "icelsuarez1",
+					"name": "icel suarez",
+					"id": 1186244193741607000,
+					"id_str": "1186244193741606912",
+					"indices": [
+						93,
+						105
+					]
+				},
+				{
+					"screen_name": "DrSarahToddUHDB",
+					"name": "Sarah Todd",
+					"id": 1110170256243388400,
+					"id_str": "1110170256243388417",
+					"indices": [
+						106,
+						122
+					]
+				},
+				{
+					"screen_name": "MaheySangeeta",
+					"name": "Sangeeta Mahey",
+					"id": 1144244650972258300,
+					"id_str": "1144244650972258305",
+					"indices": [
+						286,
+						300
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1431352421482180600,
+		"in_reply_to_status_id_str": "1431352421482180615",
+		"in_reply_to_user_id": 1144244650972258300,
+		"in_reply_to_user_id_str": "1144244650972258305",
+		"in_reply_to_screen_name": "MaheySangeeta",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust & @suaupdates | Chair - Armed Forces Network (UHDB) | Professional Committee @thecsp | @HLA_int Scholar | #150Leaders",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2968,
+			"friends_count": 4666,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7472,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1602745017",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MaheySangeeta @DanylJoe @gavinUHDB @harinder_d @SharonExtance @alisonthorp @mwansamulenga03 @icelsuarez1 @DrSarahToddUHDB “Challenging the status quo takes commitment, courage, imagination, and, above all, dedication to learning.” - Marshall Ganz ✨\n\nThank you for being the powerhouse @MaheySangeeta"
+	},
+	{
 		"created_at": "Fri Aug 27 08:59:39 +0000 2021",
 		"id": 1431179592690290700,
 		"id_str": "1431179592690290689",
