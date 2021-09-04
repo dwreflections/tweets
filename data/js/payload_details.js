58a30e3e1fa25a364ddc9e79e2f5bd3c0357da3a
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-09-04 02:17:34 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7036,
-	"created_at": "Fri Sep 03 01:17:29 +0000 2021",
+	"tweets": 7038,
+	"created_at": "Sat Sep 04 01:17:32 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 03 16:35:27 +0000 2021",
+		"id": 1433831013029978000,
+		"id_str": "1433831013029978113",
+		"full_text": "Best of luck with your studies &amp; career Roy.\n65 years young ⬇️ @thecspstudents \n\n@uow_physio @AirborneMedics @UHDB_AFN @DMS_MilMed https://t.co/msrUls3JN4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thecspstudents",
+					"name": "CSP Students",
+					"id": 444648690,
+					"id_str": "444648690",
+					"indices": [
+						67,
+						82
+					]
+				},
+				{
+					"screen_name": "UoW_Physio",
+					"name": "Uni of Winch Physio",
+					"id": 984028687396954100,
+					"id_str": "984028687396954119",
+					"indices": [
+						85,
+						96
+					]
+				},
+				{
+					"screen_name": "AirborneMedics",
+					"name": "16 Medical Regiment",
+					"id": 993937265809395700,
+					"id_str": "993937265809395712",
+					"indices": [
+						97,
+						112
+					]
+				},
+				{
+					"screen_name": "UHDB_AFN",
+					"name": "UHDB Armed Forces Network",
+					"id": 1384135578233360400,
+					"id_str": "1384135578233360399",
+					"indices": [
+						113,
+						122
+					]
+				},
+				{
+					"screen_name": "DMS_MilMed",
+					"name": "Defence Medical Services",
+					"id": 736186559888789500,
+					"id_str": "736186559888789504",
+					"indices": [
+						123,
+						134
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1433831006394634200,
+					"id_str": "1433831006394634243",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E-X9tBwX0AMxjVE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-X9tBwX0AMxjVE.jpg",
+					"url": "https://t.co/msrUls3JN4",
+					"display_url": "pic.twitter.com/msrUls3JN4",
+					"expanded_url": "https://twitter.com/beardedPT/status/1433831013029978113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1433831006394634200,
+					"id_str": "1433831006394634243",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E-X9tBwX0AMxjVE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-X9tBwX0AMxjVE.jpg",
+					"url": "https://t.co/msrUls3JN4",
+					"display_url": "pic.twitter.com/msrUls3JN4",
+					"expanded_url": "https://twitter.com/beardedPT/status/1433831013029978113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust &@suaupdates | Chair - Armed Forces Network (UHDB) |Professional Committee @thecsp |@HLA_int Scholar & #150Leaders alumni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2979,
+			"friends_count": 4692,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23484,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7481,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1630447707",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Best of luck with your studies &amp; career Roy.\n65 years young ⬇️ @thecspstudents \n\n@uow_physio @AirborneMedics @UHDB_AFN @DMS_MilMed https://t.co/msrUls3JN4"
+	},
+	{
+		"created_at": "Fri Sep 03 15:30:17 +0000 2021",
+		"id": 1433814611426414600,
+		"id_str": "1433814611426414597",
+		"full_text": "@QI_Bookclub @Becky_QICTM @AHPbookClub Yep. Grows by the day… 📚 \nI have just finished Becoming the Change. It’s a must read - if you haven’t already.",
+		"truncated": false,
+		"display_text_range": [
+			39,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "QI_Bookclub",
+					"name": "QI Book Club",
+					"id": 1137793564098859000,
+					"id_str": "1137793564098859008",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "Becky_QICTM",
+					"name": "Becky (she/her)",
+					"id": 3360257014,
+					"id_str": "3360257014",
+					"indices": [
+						13,
+						25
+					]
+				},
+				{
+					"screen_name": "AHPbookClub",
+					"name": "AHPbookClub",
+					"id": 1333729451679621000,
+					"id_str": "1333729451679621120",
+					"indices": [
+						26,
+						38
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1433773947632423000,
+		"in_reply_to_status_id_str": "1433773947632422918",
+		"in_reply_to_user_id": 1137793564098859000,
+		"in_reply_to_user_id_str": "1137793564098859008",
+		"in_reply_to_screen_name": "QI_Bookclub",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust &@suaupdates | Chair - Armed Forces Network (UHDB) |Professional Committee @thecsp |@HLA_int Scholar & #150Leaders alumni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2979,
+			"friends_count": 4692,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23484,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7481,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1630447707",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@QI_Bookclub @Becky_QICTM @AHPbookClub Yep. Grows by the day… 📚 \nI have just finished Becoming the Change. It’s a must read - if you haven’t already."
+	},
+	{
 		"created_at": "Thu Sep 02 12:45:37 +0000 2021",
 		"id": 1433410787712843800,
 		"id_str": "1433410787712843776",
