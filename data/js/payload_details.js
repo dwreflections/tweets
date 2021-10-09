16fefec829a3197f7c7d738f9aeee07ca7fd1cea
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-10-09 02:17:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7095,
-	"created_at": "Fri Oct 08 01:17:31 +0000 2021",
+	"tweets": 7096,
+	"created_at": "Sat Oct 09 01:17:32 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 08 13:04:59 +0000 2021",
+		"id": 1446461624337014800,
+		"id_str": "1446461624337014785",
+		"full_text": "@Ruuqie @culturalhealth_ @LPTnhs Huge congratulations 👏👍",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Ruuqie",
+					"name": "Farouk 'Rook' Bello",
+					"id": 603167025,
+					"id_str": "603167025",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "culturalhealth_",
+					"name": "Cultural Health Club",
+					"id": 1254809652774211600,
+					"id_str": "1254809652774211584",
+					"indices": [
+						8,
+						24
+					]
+				},
+				{
+					"screen_name": "LPTnhs",
+					"name": "Leicestershire Partnership NHS Trust",
+					"id": 562737335,
+					"id_str": "562737335",
+					"indices": [
+						25,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1446451032805068800,
+		"in_reply_to_status_id_str": "1446451032805068804",
+		"in_reply_to_user_id": 603167025,
+		"in_reply_to_user_id_str": "603167025",
+		"in_reply_to_screen_name": "Ruuqie",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust & @suaupdates | Chair - Armed Forces Network (UHDB) | Professional Committee @thecsp |@HLA_int & #150Leaders alumni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3058,
+			"friends_count": 4810,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23771,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7548,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1630447707",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Ruuqie @culturalhealth_ @LPTnhs Huge congratulations 👏👍"
+	},
+	{
 		"created_at": "Thu Oct 07 18:43:30 +0000 2021",
 		"id": 1446176797218033700,
 		"id_str": "1446184424631062532",
