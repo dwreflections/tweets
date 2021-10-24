5348c1b3a3dc998b87d8e84928932ffcfb0b28c9
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-10-24 02:17:30 +0100

    Added 1 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 23 17:04:57 +0000 2021",
+		"id": 1451957832463659000,
+		"id_str": "1451957832463659012",
+		"full_text": "@RabeeaAhmad1 @UoS_Physio @WLCPhysio",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RabeeaAhmad1",
+					"name": "Rabeea Ahmad🇵🇰🇬🇧🧕",
+					"id": 1316835449277755400,
+					"id_str": "1316835449277755410",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "UoS_Physio",
+					"name": "University of Salford Physiotherapy",
+					"id": 1272888059416252400,
+					"id_str": "1272888059416252419",
+					"indices": [
+						14,
+						25
+					]
+				},
+				{
+					"screen_name": "WLCPhysio",
+					"name": "Warren (He/Him)",
+					"id": 2940538043,
+					"id_str": "2940538043",
+					"indices": [
+						26,
+						36
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1451606549525418000,
+		"in_reply_to_status_id_str": "1451606549525417988",
+		"in_reply_to_user_id": 1316835449277755400,
+		"in_reply_to_user_id_str": "1316835449277755410",
+		"in_reply_to_screen_name": "RabeeaAhmad1",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "Staffordshire",
+			"description": "Physio & #CNJF | Governor: @UHDBTrust & @suaupdates | Chair - Armed Forces Network (UHDB) | Professional Committee @thecsp |@HLA_int & #150Leaders alumni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3066,
+			"friends_count": 4843,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 23888,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7575,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330522478872064002/t6N5vc-i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1630447707",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@RabeeaAhmad1 @UoS_Physio @WLCPhysio"
+	},
+	{
 		"created_at": "Fri Oct 22 18:44:22 +0000 2021",
 		"id": 1451497395016044500,
 		"id_str": "1451620463134773268",
