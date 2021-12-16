0a832ab32f7d4865c481b88475fd6ba2ab346a6c
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-12-16 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7227,
-	"created_at": "Wed Dec 15 01:17:29 +0000 2021",
+	"tweets": 7228,
+	"created_at": "Thu Dec 16 01:17:30 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Wed Dec 15 17:44:44 +0000 2021",
+		"id": 1471174399944503300,
+		"id_str": "1471174399944503299",
+		"full_text": "@abellbentley @h_carter43",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "abellbentley",
+					"name": "Amelia",
+					"id": 469375429,
+					"id_str": "469375429",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "h_carter43",
+					"name": "Hayley Carter",
+					"id": 4311628107,
+					"id_str": "4311628107",
+					"indices": [
+						14,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1471173558520926200,
+		"in_reply_to_status_id_str": "1471173558520926220",
+		"in_reply_to_user_id": 469375429,
+		"in_reply_to_user_id_str": "469375429",
+		"in_reply_to_screen_name": "abellbentley",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Rugeley 🏡 / UBirm📚",
+			"description": "Physio; Mostly walking my greyhound; many committees; post grad student; Often tweets: #AHP, #leadership, health/public policy; Interest: ☕️, politics & society",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3120,
+			"friends_count": 5001,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 24223,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1465434638231523328/w6845w7A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1465434638231523328/w6845w7A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1638010813",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@abellbentley @h_carter43"
+	},
+	{
 		"created_at": "Tue Dec 14 20:37:38 +0000 2021",
 		"id": 1470855524539326500,
 		"id_str": "1470855524539326466",
@@ -1319,7 +1423,7 @@ Grailbird.data.tweets_2021_12 =
 				"name": "Aldarico Jr Velasco",
 				"screen_name": "aldaricojr",
 				"location": "Derby, England",
-				"description": "Digital Nurse Specialist at University Hospitals of Derby and Burton NHS Foundation Trust, 🏳️‍🌈",
+				"description": "Digital Nurse Specialist at University Hospitals of Derby and Burton NHS Foundation Trust, 🏳️🌈",
 				"url": "https://t.co/OLwAhEsXJs",
 				"entities": {
 					"url": {
@@ -1912,7 +2016,7 @@ Grailbird.data.tweets_2021_12 =
 				},
 				{
 					"screen_name": "staceylousuth80",
-					"name": "Staceylou Suth 🌈👨‍👩‍👦 ♥️🧡💛💚💙💜",
+					"name": "Staceylou Suth 🌈👨👩👦 ♥️🧡💛💚💙💜",
 					"id": 2765497290,
 					"id_str": "2765497290",
 					"indices": [
