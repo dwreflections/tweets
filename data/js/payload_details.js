04142f1668153cc9ab01db615979816da09bdddc
dwreflections <50467496+dwreflections@users.noreply.github.com>
2021-12-19 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7230,
-	"created_at": "Sat Dec 18 01:17:29 +0000 2021",
+	"tweets": 7231,
+	"created_at": "Sun Dec 19 01:17:29 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sat Dec 18 13:43:50 +0000 2021",
+		"id": 1472200937955549200,
+		"id_str": "1472200937955549192",
+		"full_text": "@harryclax I have two out of the 4… 👍",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "harryclax",
+					"name": "Harry Claireaux",
+					"id": 2262187566,
+					"id_str": "2262187566",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1472197966664544300,
+		"in_reply_to_status_id_str": "1472197966664544256",
+		"in_reply_to_user_id": 2262187566,
+		"in_reply_to_user_id_str": "2262187566",
+		"in_reply_to_screen_name": "harryclax",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Rugeley 🏡 / UBirm📚",
+			"description": "Physio; Mostly walking my greyhound; many committees; post grad student; Often tweets: #AHP, #leadership, health/public policy; Interest: ☕️, politics & society",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3118,
+			"friends_count": 5000,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 24241,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7711,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1472278134489759753/QQbxVlu8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1472278134489759753/QQbxVlu8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1638010813",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@harryclax I have two out of the 4… 👍"
+	},
+	{
 		"created_at": "Fri Dec 17 12:26:39 +0000 2021",
 		"id": 1471819127979487200,
 		"id_str": "1471819127979487248",
