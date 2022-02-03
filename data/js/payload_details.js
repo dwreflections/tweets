872dbf782e8462cb9326381e8d2e7ba17450e549
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-02-03 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7346,
-	"created_at": "Tue Feb 01 01:17:29 +0000 2022",
+	"tweets": 7347,
+	"created_at": "Thu Feb 03 01:17:29 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Wed Feb 02 21:49:46 +0000 2022",
+		"id": 1488908804972548000,
+		"id_str": "1488993068560924682",
+		"full_text": "Reading the levelling up white paper (or book at 332 pages) and tweeting some bits.  This for example - noone could argue they are not taking a long term view of levelling up... https://t.co/a8l6ClZaCZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1488908165756469200,
+					"id_str": "1488908165756469257",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FKmqFpMXwAk9cl-.png",
+					"media_url_https": "https://pbs.twimg.com/media/FKmqFpMXwAk9cl-.png",
+					"url": "https://t.co/a8l6ClZaCZ",
+					"display_url": "pic.twitter.com/a8l6ClZaCZ",
+					"expanded_url": "https://twitter.com/kieran_walshe/status/1488908804972548105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488908165756469200,
+					"id_str": "1488908165756469257",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FKmqFpMXwAk9cl-.png",
+					"media_url_https": "https://pbs.twimg.com/media/FKmqFpMXwAk9cl-.png",
+					"url": "https://t.co/a8l6ClZaCZ",
+					"display_url": "pic.twitter.com/a8l6ClZaCZ",
+					"expanded_url": "https://twitter.com/kieran_walshe/status/1488908804972548105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 530,
+							"h": 662,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 256911319,
+			"id_str": "256911319",
+			"name": "Kieran Walshe",
+			"screen_name": "kieran_walshe",
+			"location": "",
+			"description": "Prof at Univ of Manchester. Research health reform, quality, evidence - ideas not ideologies. Ultrarunner. Citizen of UK, Ireland & Europe. Son of an immigrant.",
+			"url": "https://t.co/2NvtjRwMoK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2NvtjRwMoK",
+							"expanded_url": "http://www.mbs.ac.uk/research/people/profiles/kieran.walshe",
+							"display_url": "mbs.ac.uk/research/peopl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7170,
+			"friends_count": 292,
+			"listed_count": 157,
+			"created_at": "Thu Feb 24 09:44:41 +0000 2011",
+			"favourites_count": 4893,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5567,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/459688624225673216/A6cTlj19_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/459688624225673216/A6cTlj19_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/256911319/1431952773",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Reading the levelling up white paper (or book at 332 pages) and tweeting some bits.  This for example - noone could argue they are not taking a long term view of levelling up... https://t.co/a8l6ClZaCZ"
+	}
+]
