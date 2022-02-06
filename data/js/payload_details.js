f47d23fe7ef30fd0357a4f5b2acb3bbf3a013045
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-02-06 01:17:29 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7350,
-	"created_at": "Fri Feb 04 01:17:29 +0000 2022",
+	"tweets": 7352,
+	"created_at": "Sun Feb 06 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,273 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 05 23:04:24 +0000 2022",
+		"id": 1490099016822046700,
+		"id_str": "1490099016822046724",
+		"full_text": "@TurtonsOther https://t.co/NrraEsK3kv",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TurtonsOther",
+					"name": "Pete Turton’s Other Account",
+					"id": 794599144107216900,
+					"id_str": "794599144107216898",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1490099013160427500,
+					"id_str": "1490099013160427525",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FK3lKFeWYAUYMxP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK3lKFeWYAUYMxP.jpg",
+					"url": "https://t.co/NrraEsK3kv",
+					"display_url": "pic.twitter.com/NrraEsK3kv",
+					"expanded_url": "https://twitter.com/beardedPT/status/1490099016822046724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1490099013160427500,
+					"id_str": "1490099013160427525",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FK3lKFeWYAUYMxP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK3lKFeWYAUYMxP.jpg",
+					"url": "https://t.co/NrraEsK3kv",
+					"display_url": "pic.twitter.com/NrraEsK3kv",
+					"expanded_url": "https://twitter.com/beardedPT/status/1490099016822046724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1490085975871180800,
+		"in_reply_to_status_id_str": "1490085975871180801",
+		"in_reply_to_user_id": 794599144107216900,
+		"in_reply_to_user_id_str": "794599144107216898",
+		"in_reply_to_screen_name": "TurtonsOther",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Rugeley 🏡 / UoB 📚",
+			"description": "Mostly walking my greyhound; many committees; postgrad student; Often tweets: #AHP, #Leadership, health/public policy; Interest: ☕️, veterans/defence & politics",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3150,
+			"friends_count": 4251,
+			"listed_count": 0,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 24728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7862,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1640981133",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@TurtonsOther https://t.co/NrraEsK3kv"
+	},
+	{
+		"created_at": "Sat Feb 05 22:57:01 +0000 2022",
+		"id": 1490097157952749600,
+		"id_str": "1490097157952749568",
+		"full_text": "@maj_retd_fox TTQ",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "maj_retd_fox",
+					"name": "Andrew Fox",
+					"id": 1085555771918696400,
+					"id_str": "1085555771918696449",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1490094019199877000,
+		"in_reply_to_status_id_str": "1490094019199877128",
+		"in_reply_to_user_id": 1085555771918696400,
+		"in_reply_to_user_id_str": "1085555771918696449",
+		"in_reply_to_screen_name": "maj_retd_fox",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Rugeley 🏡 / UoB 📚",
+			"description": "Mostly walking my greyhound; many committees; postgrad student; Often tweets: #AHP, #Leadership, health/public policy; Interest: ☕️, veterans/defence & politics",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3150,
+			"friends_count": 4251,
+			"listed_count": 0,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 24728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7862,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1640981133",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@maj_retd_fox TTQ"
+	},
+	{
 		"created_at": "Thu Feb 03 22:27:39 +0000 2022",
 		"id": 1489364991975252000,
 		"id_str": "1489364991975251969",
