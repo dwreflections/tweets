d4106e26dc868926b248facc919af73a4ae44a92
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-03-22 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7476,
-	"created_at": "Mon Mar 21 01:17:29 +0000 2022",
+	"tweets": 7477,
+	"created_at": "Tue Mar 22 01:17:30 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 21 17:34:42 +0000 2022",
+		"id": 1505961110121468000,
+		"id_str": "1505961110121467904",
+		"full_text": "@debs1_wilson @alicrewe @MarriottHelen Fab! 🤩👍",
+		"truncated": false,
+		"display_text_range": [
+			39,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "debs1_wilson",
+					"name": "Deborah wilson💙",
+					"id": 3115697217,
+					"id_str": "3115697217",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "alicrewe",
+					"name": "ali naylor née crewesmith",
+					"id": 330511872,
+					"id_str": "330511872",
+					"indices": [
+						14,
+						23
+					]
+				},
+				{
+					"screen_name": "MarriottHelen",
+					"name": "Helen Marriott",
+					"id": 816853278,
+					"id_str": "816853278",
+					"indices": [
+						24,
+						38
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1505942808707932200,
+		"in_reply_to_status_id_str": "1505942808707932169",
+		"in_reply_to_user_id": 3115697217,
+		"in_reply_to_user_id_str": "3115697217",
+		"in_reply_to_screen_name": "debs1_wilson",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🇺🇦",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Staffs. 🏡 / UBirm📚",
+			"description": "Greyhound Dad | Physio ➡️ Digital Health Care | Lead @UHDB_AFN | Governor @UHDBtrust | Post-grad Student @hsmc_ | Professional Committee @theCSP |",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3170,
+			"friends_count": 4330,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 25162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8009,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1640981133",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@debs1_wilson @alicrewe @MarriottHelen Fab! 🤩👍"
+	},
+	{
 		"created_at": "Sun Mar 20 20:08:46 +0000 2022",
 		"id": 1505624623442022400,
 		"id_str": "1505637491910529025",
