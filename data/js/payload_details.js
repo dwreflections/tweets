312c722800c3cc111040d081571669767bbeb1cf
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-04-11 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7575,
-	"created_at": "Sun Apr 10 01:17:30 +0000 2022",
+	"tweets": 7576,
+	"created_at": "Mon Apr 11 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sun Apr 10 10:38:12 +0000 2022",
+		"id": 1512453352998719500,
+		"id_str": "1513104050522370055",
+		"full_text": "*NEWS ALERT* @nhsengland has launched a survey to find out people's views and experiences of veterans mental and physical health services in England. Complete the survey at https://t.co/54TVoyA2a8 to help inform future NHS veterans services in England #OPCOURAGE #veterans",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OPCOURAGE",
+					"indices": [
+						252,
+						262
+					]
+				},
+				{
+					"text": "veterans",
+					"indices": [
+						263,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NHSEngland",
+					"name": "NHS England and NHS Improvement",
+					"id": 551426814,
+					"id_str": "551426814",
+					"indices": [
+						13,
+						24
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/54TVoyA2a8",
+					"expanded_url": "https://www.engage.england.nhs.uk/survey/improving-veterans-mental-and-physical-health/",
+					"display_url": "engage.england.nhs.uk/survey/improvi…",
+					"indices": [
+						173,
+						196
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1428396689111531500,
+			"id_str": "1428396689111531534",
+			"name": "NHS Armed Forces Health",
+			"screen_name": "NHSArmedForces",
+			"location": "",
+			"description": "Official NHS England Armed Forces health, signposting and service information. Account not monitored 24/7. For emergency or crisis response call NHS 111 or 999.",
+			"url": "https://t.co/JQmTndWHNr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JQmTndWHNr",
+							"expanded_url": "http://www.nhs.uk/armedforceshealth",
+							"display_url": "nhs.uk/armedforceshea…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1248,
+			"friends_count": 146,
+			"listed_count": 2,
+			"created_at": "Thu Aug 19 16:41:48 +0000 2021",
+			"favourites_count": 47,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 215,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1428397186094714880/v12d691E_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1428397186094714880/v12d691E_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1428396689111531534/1629907719",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 79,
+		"favorite_count": 57,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "*NEWS ALERT* @nhsengland has launched a survey to find out people's views and experiences of veterans mental and physical health services in England. Complete the survey at https://t.co/54TVoyA2a8 to help inform future NHS veterans services in England #OPCOURAGE #veterans"
+	},
+	{
 		"created_at": "Sat Apr 09 11:31:56 +0000 2022",
 		"id": 1512755184597876700,
 		"id_str": "1512755184597876746",
