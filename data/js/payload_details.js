74bd70bdf11f859b68ee61e626d129c5be73c361
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-04-12 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7576,
-	"created_at": "Mon Apr 11 01:17:29 +0000 2022",
+	"tweets": 7577,
+	"created_at": "Tue Apr 12 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Mon Apr 11 17:47:12 +0000 2022",
+		"id": 1513574398074179600,
+		"id_str": "1513574398074179593",
+		"full_text": "It was great to present @UHDB_AFN to @DivisionUhdb this morning.\n\nMy take away messages was to be curious of your workforce especially those who are #veterans, reservists, cadet leaders and military spouses. https://t.co/NdJFXVUyGH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "veterans",
+					"indices": [
+						149,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UHDB_AFN",
+					"name": "UHDB Armed Forces Network",
+					"id": 1384135578233360400,
+					"id_str": "1384135578233360399",
+					"indices": [
+						24,
+						33
+					]
+				},
+				{
+					"screen_name": "DivisionUhdb",
+					"name": "UHDB Medicine Division",
+					"id": 1201614306531586000,
+					"id_str": "1201614306531586049",
+					"indices": [
+						37,
+						50
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1513574394358112300,
+					"id_str": "1513574394358112261",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/FQFL5HTXoAUfNUD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQFL5HTXoAUfNUD.jpg",
+					"url": "https://t.co/NdJFXVUyGH",
+					"display_url": "pic.twitter.com/NdJFXVUyGH",
+					"expanded_url": "https://twitter.com/beardedPT/status/1513574398074179593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 906,
+							"h": 668,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 906,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 501,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1513574394358112300,
+					"id_str": "1513574394358112261",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/FQFL5HTXoAUfNUD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQFL5HTXoAUfNUD.jpg",
+					"url": "https://t.co/NdJFXVUyGH",
+					"display_url": "pic.twitter.com/NdJFXVUyGH",
+					"expanded_url": "https://twitter.com/beardedPT/status/1513574398074179593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 906,
+							"h": 668,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 906,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 501,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams 🇺🇦",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Staffs. 🏡 / UBirm📚",
+			"description": "Greyhound Dad | Physio ➡️ Digital Health Care | Lead @UHDB_AFN | Governor @UHDBtrust | Post-grad Student @hsmc_ | Professional Committee @theCSP |",
+			"url": "https://t.co/woJ8pdZGWH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/woJ8pdZGWH",
+							"expanded_url": "https://www.linkedin.com/in/ukdavidwilliams",
+							"display_url": "linkedin.com/in/ukdavidwill…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3180,
+			"friends_count": 4373,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 25456,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8124,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477008040108994566/PRRNfWjv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1640981133",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It was great to present @UHDB_AFN to @DivisionUhdb this morning.\n\nMy take away messages was to be curious of your workforce especially those who are #veterans, reservists, cadet leaders and military spouses. https://t.co/NdJFXVUyGH"
+	},
+	{
 		"created_at": "Sun Apr 10 10:38:12 +0000 2022",
 		"id": 1512453352998719500,
 		"id_str": "1513104050522370055",
