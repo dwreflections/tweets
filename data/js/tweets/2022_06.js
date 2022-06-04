f1ada9fb822467b1b362c6f3ccfe60a71ad03a3b
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-06-04 02:17:30 +0100

    Added 3 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,442 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 03 21:05:24 +0000 2022",
+		"id": 1532830834264354800,
+		"id_str": "1532830834264354819",
+		"full_text": "@nicklivadas @FlippinPain 🥰😍",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nicklivadas",
+					"name": "Nick Livadas",
+					"id": 375320758,
+					"id_str": "375320758",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "FlippinPain",
+					"name": "Flippin’ Pain",
+					"id": 1204725688982495200,
+					"id_str": "1204725688982495233",
+					"indices": [
+						13,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1532716397188505600,
+		"in_reply_to_status_id_str": "1532716397188505600",
+		"in_reply_to_user_id": 375320758,
+		"in_reply_to_user_id_str": "375320758",
+		"in_reply_to_screen_name": "nicklivadas",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care & Leadership | Lead @UHDB_AFN | Governor @UHDBtrust @suaupdates | MSc Student @_HSMCentre | Vice-Chair ~ Professional Com. @theCSP",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3220,
+			"friends_count": 4459,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26093,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1530588662139916289/ohAJRhaC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1530588662139916289/ohAJRhaC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1653991445",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@nicklivadas @FlippinPain 🥰😍"
+	},
+	{
+		"created_at": "Fri Jun 03 13:19:16 +0000 2022",
+		"id": 1532713530268459000,
+		"id_str": "1532713530268459009",
+		"full_text": "@nicklivadas I didn’t know you was doing a PhD @nicklivadas 👍😲😬",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nicklivadas",
+					"name": "Nick Livadas",
+					"id": 375320758,
+					"id_str": "375320758",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "nicklivadas",
+					"name": "Nick Livadas",
+					"id": 375320758,
+					"id_str": "375320758",
+					"indices": [
+						47,
+						59
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1532711283857973200,
+		"in_reply_to_status_id_str": "1532711283857973251",
+		"in_reply_to_user_id": 375320758,
+		"in_reply_to_user_id_str": "375320758",
+		"in_reply_to_screen_name": "nicklivadas",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care & Leadership | Lead @UHDB_AFN | Governor @UHDBtrust @suaupdates | MSc Student @_HSMCentre | Vice-Chair ~ Professional Com. @theCSP",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3220,
+			"friends_count": 4459,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26093,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1530588662139916289/ohAJRhaC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1530588662139916289/ohAJRhaC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1653991445",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@nicklivadas I didn’t know you was doing a PhD @nicklivadas 👍😲😬"
+	},
+	{
+		"created_at": "Fri Jun 03 11:03:39 +0000 2022",
+		"id": 1532386014810996700,
+		"id_str": "1532679399111372802",
+		"full_text": "Video from the cockpit: Putting on the red, white and blue over Buckingham Palace for the #PlatinumJubilee \nThe #RedArrows were honoured to perform for Her Majesty The Queen and the huge crowds both on the ground in #London and watching from around the world this afternoon. https://t.co/cnFGFZPsO8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PlatinumJubilee",
+					"indices": [
+						90,
+						106
+					]
+				},
+				{
+					"text": "RedArrows",
+					"indices": [
+						112,
+						122
+					]
+				},
+				{
+					"text": "London",
+					"indices": [
+						216,
+						223
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1532385761055629300,
+					"id_str": "1532385761055629313",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532385761055629313/pu/img/KNsC_Sj_pxZRO8c-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532385761055629313/pu/img/KNsC_Sj_pxZRO8c-.jpg",
+					"url": "https://t.co/cnFGFZPsO8",
+					"display_url": "pic.twitter.com/cnFGFZPsO8",
+					"expanded_url": "https://twitter.com/rafredarrows/status/1532386014810996736/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532385761055629300,
+					"id_str": "1532385761055629313",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532385761055629313/pu/img/KNsC_Sj_pxZRO8c-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532385761055629313/pu/img/KNsC_Sj_pxZRO8c-.jpg",
+					"url": "https://t.co/cnFGFZPsO8",
+					"display_url": "pic.twitter.com/cnFGFZPsO8",
+					"expanded_url": "https://twitter.com/rafredarrows/status/1532386014810996736/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 34720,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532385761055629313/pu/vid/640x360/Azp5Wo65xrc7pVHP.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532385761055629313/pu/vid/480x270/Bc_RQJ90wVD_YxqP.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1532385761055629313/pu/pl/GneQpk3gkarHWGKy.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532385761055629313/pu/vid/1280x720/e0_jvZkxWXhd-kum.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 587397179,
+			"id_str": "587397179",
+			"name": "Red Arrows",
+			"screen_name": "rafredarrows",
+			"location": "",
+			"description": "With precision displays, the Royal Air Force Aerobatic Team, the Red Arrows, showcases the excellence of the RAF and represents the UK at home and overseas.",
+			"url": "https://t.co/sD8IcsGAui",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sD8IcsGAui",
+							"expanded_url": "https://linktr.ee/redarrows22",
+							"display_url": "linktr.ee/redarrows22",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 213356,
+			"friends_count": 1098,
+			"listed_count": 700,
+			"created_at": "Tue May 22 12:48:07 +0000 2012",
+			"favourites_count": 17843,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9134,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1460288772642426880/QMvlkSYs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1460288772642426880/QMvlkSYs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/587397179/1618824676",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1581,
+		"favorite_count": 10954,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Video from the cockpit: Putting on the red, white and blue over Buckingham Palace for the #PlatinumJubilee \nThe #RedArrows were honoured to perform for Her Majesty The Queen and the huge crowds both on the ground in #London and watching from around the world this afternoon. https://t.co/cnFGFZPsO8"
+	},
+	{
 		"created_at": "Thu Jun 02 12:45:38 +0000 2022",
 		"id": 1532342678981169200,
 		"id_str": "1532342678981169153",
