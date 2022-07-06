2b4f26daea536ba6c9787e25562983c184239fdd
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-07-06 02:17:30 +0100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7793,
-	"created_at": "Wed Jun 29 01:17:29 +0000 2022",
+	"tweets": 7797,
+	"created_at": "Wed Jul 06 01:17:29 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,529 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Tue Jul 05 20:24:35 +0000 2022",
+		"id": 1544416975962755000,
+		"id_str": "1544416975962755076",
+		"full_text": "@RogerKerry1 Huge opportunity! Get out of those silos 🤩",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RogerKerry1",
+					"name": "Roger Kerry",
+					"id": 396832258,
+					"id_str": "396832258",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1544409091719807000,
+		"in_reply_to_status_id_str": "1544409091719806977",
+		"in_reply_to_user_id": 396832258,
+		"in_reply_to_user_id_str": "396832258",
+		"in_reply_to_screen_name": "RogerKerry1",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care & Leadership | Lead @UHDB_AFN | Governor @UHDBtrust @suaupdates | Student @_HSMCentre | Vice-Chair ~ Professional Com. @theCSP",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3263,
+			"friends_count": 4534,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26454,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1653991445",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@RogerKerry1 Huge opportunity! Get out of those silos 🤩"
+	},
+	{
+		"created_at": "Tue Jul 05 16:01:24 +0000 2022",
+		"id": 1544224644164722700,
+		"id_str": "1544350742550544384",
+		"full_text": "‘New local partnerships risk being undermined by national policy choices beyond their control, including insufficient funding for local government and public health services’\n#IntegratedCareSystems ⁦⁦@bmj_latest⁩  https://t.co/AMPneCW6IV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IntegratedCareSystems",
+					"indices": [
+						175,
+						197
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bmj_latest",
+					"name": "The BMJ",
+					"id": 16949344,
+					"id_str": "16949344",
+					"indices": [
+						200,
+						211
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AMPneCW6IV",
+					"expanded_url": "https://www.bmj.com/content/378/bmj-2022-070910",
+					"display_url": "bmj.com/content/378/bm…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 748793098998734800,
+			"id_str": "748793098998734848",
+			"name": "Dr Pauline Nelson 👓",
+			"screen_name": "DrP_Manc",
+			"location": "Manchester, Europe",
+			"description": "Health service & policy qualitative researcher; suicide bereavement; European; Tea drinker; Armchair cyclist; Music lover 🎶",
+			"url": "https://t.co/RoOcozNCKo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RoOcozNCKo",
+							"expanded_url": "http://www.researchgate.net/profile/Pauline_Nelson",
+							"display_url": "researchgate.net/profile/Paulin…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 848,
+			"friends_count": 1966,
+			"listed_count": 15,
+			"created_at": "Fri Jul 01 08:19:21 +0000 2016",
+			"favourites_count": 24574,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10223,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501679865732509700/CGRLKrnK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501679865732509700/CGRLKrnK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/748793098998734848/1615762220",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 15,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "‘New local partnerships risk being undermined by national policy choices beyond their control, including insufficient funding for local government and public health services’\n#IntegratedCareSystems ⁦⁦@bmj_latest⁩  https://t.co/AMPneCW6IV"
+	},
+	{
+		"created_at": "Tue Jul 05 15:54:22 +0000 2022",
+		"id": 1544348973347270700,
+		"id_str": "1544348973347270656",
+		"full_text": "@mjandison @Fed_IP @DHSummerSchools @DrJamesReed @EMAHPInfo @Ramandeep_kaur9 @SarahNewcombe22 Unfortunately, I could not make it. See you all next week. #DHSS2022 🙌",
+		"truncated": false,
+		"display_text_range": [
+			94,
+			164
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DHSS2022",
+					"indices": [
+						153,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mjandison",
+					"name": "Melissa Andison 🌿🚲🇦🇺🇬🇧",
+					"id": 476773026,
+					"id_str": "476773026",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "Fed_IP",
+					"name": "FEDIP-The Federation for Informatics Professionals",
+					"id": 1100711991344279600,
+					"id_str": "1100711991344279553",
+					"indices": [
+						11,
+						18
+					]
+				},
+				{
+					"screen_name": "DHSummerSchools",
+					"name": "Digital Health Summer Schools",
+					"id": 984801770533802000,
+					"id_str": "984801770533801985",
+					"indices": [
+						19,
+						35
+					]
+				},
+				{
+					"screen_name": "DrJamesReed",
+					"name": "James Reed",
+					"id": 1598260682,
+					"id_str": "1598260682",
+					"indices": [
+						36,
+						48
+					]
+				},
+				{
+					"screen_name": "EMAHPInfo",
+					"name": "Euan McComiskie",
+					"id": 2438541736,
+					"id_str": "2438541736",
+					"indices": [
+						49,
+						59
+					]
+				},
+				{
+					"screen_name": "Ramandeep_kaur9",
+					"name": "Ramandeep Kaur",
+					"id": 3202070813,
+					"id_str": "3202070813",
+					"indices": [
+						60,
+						76
+					]
+				},
+				{
+					"screen_name": "SarahNewcombe22",
+					"name": "Sarah Newcombe",
+					"id": 2676616389,
+					"id_str": "2676616389",
+					"indices": [
+						77,
+						93
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1544345790306345000,
+		"in_reply_to_status_id_str": "1544345790306344966",
+		"in_reply_to_user_id": 476773026,
+		"in_reply_to_user_id_str": "476773026",
+		"in_reply_to_screen_name": "mjandison",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care & Leadership | Lead @UHDB_AFN | Governor @UHDBtrust @suaupdates | Student @_HSMCentre | Vice-Chair ~ Professional Com. @theCSP",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3263,
+			"friends_count": 4534,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26454,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1653991445",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@mjandison @Fed_IP @DHSummerSchools @DrJamesReed @EMAHPInfo @Ramandeep_kaur9 @SarahNewcombe22 Unfortunately, I could not make it. See you all next week. #DHSS2022 🙌"
+	},
+	{
+		"created_at": "Tue Jul 05 15:44:46 +0000 2022",
+		"id": 1544346558245871600,
+		"id_str": "1544346558245871617",
+		"full_text": "@kyprmph @cspbame @thecsp @IshmaelBeckford @KMiddletonCSP @AlexMacKenzie11 Excellent!",
+		"truncated": false,
+		"display_text_range": [
+			75,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kyprmph",
+					"name": "Doreen Caesar",
+					"id": 928326405095555100,
+					"id_str": "928326405095555072",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "cspbame",
+					"name": "CSP BAME Network",
+					"id": 1278338601982529500,
+					"id_str": "1278338601982529536",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"screen_name": "thecsp",
+					"name": "Chartered Society of Physiotherapy",
+					"id": 138098880,
+					"id_str": "138098880",
+					"indices": [
+						18,
+						25
+					]
+				},
+				{
+					"screen_name": "IshmaelBeckford",
+					"name": "Ishmael Beckford (he/him) 🇯🇲🇬🇧✊🏿",
+					"id": 1271214327455010800,
+					"id_str": "1271214327455010816",
+					"indices": [
+						26,
+						42
+					]
+				},
+				{
+					"screen_name": "KMiddletonCSP",
+					"name": "Karen Middleton (she/her)",
+					"id": 791233063,
+					"id_str": "791233063",
+					"indices": [
+						43,
+						57
+					]
+				},
+				{
+					"screen_name": "AlexMacKenzie11",
+					"name": "Alex Mackenzie 💙 She/her",
+					"id": 457442958,
+					"id_str": "457442958",
+					"indices": [
+						58,
+						74
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1539517969159667700,
+		"in_reply_to_status_id_str": "1539517969159667712",
+		"in_reply_to_user_id": 928326405095555100,
+		"in_reply_to_user_id_str": "928326405095555072",
+		"in_reply_to_screen_name": "kyprmph",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care & Leadership | Lead @UHDB_AFN | Governor @UHDBtrust @suaupdates | Student @_HSMCentre | Vice-Chair ~ Professional Com. @theCSP",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3263,
+			"friends_count": 4534,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26454,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1540602067382927363/Wn1-U-p0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1653991445",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "fr",
+		"text": "@kyprmph @cspbame @thecsp @IshmaelBeckford @KMiddletonCSP @AlexMacKenzie11 Excellent!"
+	}
+]
