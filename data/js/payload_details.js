fdb1212faad8eb07074936f2b9adaa95f303bb31
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-08-02 02:17:30 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7853,
-	"created_at": "Mon Aug 01 01:17:29 +0000 2022",
+	"tweets": 7856,
+	"created_at": "Tue Aug 02 01:17:29 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_08.js
@@ -0,0 +1,431 @@
+Grailbird.data.tweets_2022_08 = 
+[
+	{
+		"created_at": "Mon Aug 01 20:51:21 +0000 2022",
+		"id": 1554208182951608300,
+		"id_str": "1554208182951608325",
+		"full_text": "@SophieECapewell @swpixtweets @TeamEngland Congratulations! A brilliant result - hard work pays off!! 💪👏",
+		"truncated": false,
+		"display_text_range": [
+			43,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SophieECapewell",
+					"name": "Sophie Capewell",
+					"id": 923315653,
+					"id_str": "923315653",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "swpixtweets",
+					"name": "SWpix.com",
+					"id": 26029779,
+					"id_str": "26029779",
+					"indices": [
+						17,
+						29
+					]
+				},
+				{
+					"screen_name": "TeamEngland",
+					"name": "Team England",
+					"id": 78975312,
+					"id_str": "78975312",
+					"indices": [
+						30,
+						42
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1553847236194336800,
+		"in_reply_to_status_id_str": "1553847236194336775",
+		"in_reply_to_user_id": 923315653,
+		"in_reply_to_user_id_str": "923315653",
+		"in_reply_to_screen_name": "SophieECapewell",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care | #DigitalAHP | Proudly Leading @UHDB_AFN | Governor @UHDBtrust @suaupdates | Studying @_HSMCentre | Proff. Com. @theCSP | 📥 Open",
+			"url": "https://t.co/Qd40HbmAVv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Qd40HbmAVv",
+							"expanded_url": "https://campsite.bio/davidww",
+							"display_url": "campsite.bio/davidww",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3308,
+			"friends_count": 4617,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26722,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1658777389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SophieECapewell @swpixtweets @TeamEngland Congratulations! A brilliant result - hard work pays off!! 💪👏"
+	},
+	{
+		"created_at": "Mon Aug 01 14:33:38 +0000 2022",
+		"id": 1554113126764880000,
+		"id_str": "1554113126764879876",
+		"full_text": "@littlejon1234 @nohandsup Page 25. 🔗: https://t.co/QpUgzQuIi1",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "littlejon1234",
+					"name": "Jonathan Hodgson",
+					"id": 2336407923,
+					"id_str": "2336407923",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "nohandsup",
+					"name": "Stephen Charleston",
+					"id": 261285355,
+					"id_str": "261285355",
+					"indices": [
+						15,
+						25
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QpUgzQuIi1",
+					"expanded_url": "https://azuksappnpdsa01.blob.core.windows.net/datashare/National-Uniform-Consultation-The-Results-Report-20-September-2021-T11.pdf",
+					"display_url": "azuksappnpdsa01.blob.core.windows.net/datashare/Nati…",
+					"indices": [
+						38,
+						61
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1554111085359366100,
+		"in_reply_to_status_id_str": "1554111085359366145",
+		"in_reply_to_user_id": 2336407923,
+		"in_reply_to_user_id_str": "2336407923",
+		"in_reply_to_screen_name": "littlejon1234",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care | #DigitalAHP | Proudly Leading @UHDB_AFN | Governor @UHDBtrust @suaupdates | Studying @_HSMCentre | Proff. Com. @theCSP | 📥 Open",
+			"url": "https://t.co/Qd40HbmAVv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Qd40HbmAVv",
+							"expanded_url": "https://campsite.bio/davidww",
+							"display_url": "campsite.bio/davidww",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3308,
+			"friends_count": 4617,
+			"listed_count": 20,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26722,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1658777389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@littlejon1234 @nohandsup Page 25. 🔗: https://t.co/QpUgzQuIi1"
+	},
+	{
+		"created_at": "Mon Aug 01 08:54:38 +0000 2022",
+		"id": 1554002736433090600,
+		"id_str": "1554027818329186304",
+		"full_text": "So you’ve got a month to get your applications in for 2 key @thecsp committees.  What’s stopping you? We need your enthusiasm, expertise &amp; commitment.  Not sure? Get in touch &amp; find out more! https://t.co/eA51lWYt0w",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thecsp",
+					"name": "Chartered Society of Physiotherapy",
+					"id": 138098880,
+					"id_str": "138098880",
+					"indices": [
+						60,
+						67
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1554002727868440600,
+					"id_str": "1554002727868440577",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FZDtP16WIAE_8-g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZDtP16WIAE_8-g.jpg",
+					"url": "https://t.co/eA51lWYt0w",
+					"display_url": "pic.twitter.com/eA51lWYt0w",
+					"expanded_url": "https://twitter.com/katiem_wilkie/status/1554002736433090562/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1250,
+							"h": 1042,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 567,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1554002727868440600,
+					"id_str": "1554002727868440577",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FZDtP16WIAE_8-g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZDtP16WIAE_8-g.jpg",
+					"url": "https://t.co/eA51lWYt0w",
+					"display_url": "pic.twitter.com/eA51lWYt0w",
+					"expanded_url": "https://twitter.com/katiem_wilkie/status/1554002736433090562/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1250,
+							"h": 1042,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 567,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2335785232,
+			"id_str": "2335785232",
+			"name": "katie wilkie 💙",
+			"screen_name": "katiem_wilkie",
+			"location": "",
+			"description": "Chartered physiotherapist working in neuro-rehabilitation. Vice Chair of CSP Council. Trade unionist. Views are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1342,
+			"friends_count": 1391,
+			"listed_count": 14,
+			"created_at": "Mon Feb 10 14:11:02 +0000 2014",
+			"favourites_count": 24150,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8711,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1345304780718170112/uyzSNRXG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1345304780718170112/uyzSNRXG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2335785232/1413475694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So you’ve got a month to get your applications in for 2 key @thecsp committees.  What’s stopping you? We need your enthusiasm, expertise &amp; commitment.  Not sure? Get in touch &amp; find out more! https://t.co/eA51lWYt0w"
+	}
+]
