e3caa77a65c8284e38b66e4f9639cbc2798cfb28
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-08-28 02:17:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7902,
-	"created_at": "Sat Aug 27 01:17:30 +0000 2022",
+	"tweets": 7903,
+	"created_at": "Sun Aug 28 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 27 08:58:54 +0000 2022",
+		"id": 1563450975192461300,
+		"id_str": "1563450975192461315",
+		"full_text": "@AliBooker_RD @ForceAtlantic https://t.co/nVPzRbZB1O",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AliBooker_RD",
+					"name": "Ali Booker 💙",
+					"id": 941762706,
+					"id_str": "941762706",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "ForceAtlantic",
+					"name": "Force Atlantic",
+					"id": 1281920918021963800,
+					"id_str": "1281920918021963781",
+					"indices": [
+						14,
+						28
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1563450967055421400,
+					"id_str": "1563450967055421441",
+					"indices": [
+						29,
+						52
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FbJ-XvsUUAE1Z26.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FbJ-XvsUUAE1Z26.jpg",
+					"url": "https://t.co/nVPzRbZB1O",
+					"display_url": "pic.twitter.com/nVPzRbZB1O",
+					"expanded_url": "https://twitter.com/beardedPT/status/1563450975192461315/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1563450967055421400,
+					"id_str": "1563450967055421441",
+					"indices": [
+						29,
+						52
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FbJ-XvsUUAE1Z26.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FbJ-XvsUUAE1Z26.jpg",
+					"url": "https://t.co/nVPzRbZB1O",
+					"display_url": "pic.twitter.com/nVPzRbZB1O",
+					"expanded_url": "https://twitter.com/beardedPT/status/1563450975192461315/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							25,
+							14
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FbJ-XvsUUAE1Z26.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1562802649703252000,
+		"in_reply_to_status_id_str": "1562802649703251969",
+		"in_reply_to_user_id": 941762706,
+		"in_reply_to_user_id_str": "941762706",
+		"in_reply_to_screen_name": "AliBooker_RD",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "Physio ➡️ Digital Health Care | #DigitalAHP | Proudly Leading @UHDB_AFN | Governor @UHDBtrust @suaupdates | Studying @_HSMCentre | Proff. Com. @theCSP | 📥 Open",
+			"url": "https://t.co/Qd40HbmAVv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Qd40HbmAVv",
+							"expanded_url": "https://campsite.bio/davidww",
+							"display_url": "campsite.bio/davidww",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3319,
+			"friends_count": 4639,
+			"listed_count": 19,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 26868,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8506,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551903467601625088/7m-8hFY9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1658777389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "qme",
+		"text": "@AliBooker_RD @ForceAtlantic https://t.co/nVPzRbZB1O"
+	},
+	{
 		"created_at": "Fri Aug 26 20:28:54 +0000 2022",
 		"id": 1563262231445352400,
 		"id_str": "1563262231445352457",
