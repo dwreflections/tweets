46578bb640ae5f092600e0be31048acb318241ed
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-11-02 01:17:39 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7953,
-	"created_at": "Fri Oct 28 01:17:29 +0000 2022",
+	"tweets": 7954,
+	"created_at": "Wed Nov 02 01:17:37 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,311 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Tue Nov 01 17:42:47 +0000 2022",
+		"id": 1587497328113500200,
+		"id_str": "1587500414899032065",
+		"full_text": "Let's make a difference across the Midlands #ReconditionTheNation Have you signed the Midlands pledge? https://t.co/TbmdvJciHc 🥇🥈🥉 @ReconGamesUK @NHSMidlands @HelenBayley3 @miriam_coffie @beardedPT @ImogenStaveley https://t.co/kthnArSVyv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ReconditionTheNation",
+					"indices": [
+						44,
+						65
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ReconGamesUK",
+					"name": "Reconditioning Games 2022 - 23",
+					"id": 1564998785968791600,
+					"id_str": "1564998785968791559",
+					"indices": [
+						131,
+						144
+					]
+				},
+				{
+					"screen_name": "NHSMidlands",
+					"name": "NHS Midlands",
+					"id": 1098892220801998800,
+					"id_str": "1098892220801998850",
+					"indices": [
+						145,
+						157
+					]
+				},
+				{
+					"screen_name": "HelenBayley3",
+					"name": "Helen Bayley",
+					"id": 863093685508067300,
+					"id_str": "863093685508067328",
+					"indices": [
+						158,
+						171
+					]
+				},
+				{
+					"screen_name": "miriam_coffie",
+					"name": "Miriam Coffie",
+					"id": 1067727040307171300,
+					"id_str": "1067727040307171328",
+					"indices": [
+						172,
+						186
+					]
+				},
+				{
+					"screen_name": "beardedPT",
+					"name": "David Williams",
+					"id": 3377750235,
+					"id_str": "3377750235",
+					"indices": [
+						187,
+						197
+					]
+				},
+				{
+					"screen_name": "ImogenStaveley",
+					"name": "Imogen Staveley",
+					"id": 2547381790,
+					"id_str": "2547381790",
+					"indices": [
+						198,
+						213
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TbmdvJciHc",
+					"expanded_url": "http://bit.ly/3TkwKmV",
+					"display_url": "bit.ly/3TkwKmV",
+					"indices": [
+						103,
+						126
+					]
+				},
+				{
+					"url": "https://t.co/kthnArSVyv",
+					"expanded_url": "https://twitter.com/ReconGamesUK/status/1587459840271695876",
+					"display_url": "twitter.com/ReconGamesUK/s…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 791640270542630900,
+			"id_str": "791640270542630913",
+			"name": "Fiona Lennon",
+			"screen_name": "fhlennon1",
+			"location": "Market Harborough, England",
+			"description": "Privileged and proud to work in the NHS. Passionate & committed to making a difference for our patients and staff. Views are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 664,
+			"friends_count": 155,
+			"listed_count": 2,
+			"created_at": "Thu Oct 27 13:58:43 +0000 2016",
+			"favourites_count": 3079,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 551,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/886868722622898176/AGY4qazU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/886868722622898176/AGY4qazU_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1587459840271696000,
+		"quoted_status_id_str": "1587459840271695876",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kthnArSVyv",
+			"expanded": "https://twitter.com/ReconGamesUK/status/1587459840271695876",
+			"display": "twitter.com/ReconGamesUK/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Nov 01 15:01:34 +0000 2022",
+			"id": 1587459840271696000,
+			"id_str": "1587459840271695876",
+			"full_text": "Thank you to everyone who joined us today for the Reconditioning Games Opening Ceremony. Over 200 colleagues committed to #ReconditionTheNation \n\nRecording and slides will be available at link below in next 24 hours.\nFollow link for more info and resources\nhttps://t.co/AznyFRnYIu https://t.co/H2FgBO2B15",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ReconditionTheNation",
+						"indices": [
+							122,
+							143
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/AznyFRnYIu",
+						"expanded_url": "https://future.nhs.uk/ECISTnetwork/view?objectID=37893328",
+						"display_url": "future.nhs.uk/ECISTnetwork/v…",
+						"indices": [
+							257,
+							280
+						]
+					},
+					{
+						"url": "https://t.co/H2FgBO2B15",
+						"expanded_url": "https://twitter.com/recongamesuk/status/1580239660210876418",
+						"display_url": "twitter.com/recongamesuk/s…",
+						"indices": [
+							281,
+							304
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1564998785968791600,
+				"id_str": "1564998785968791559",
+				"name": "Reconditioning Games 2022 - 23",
+				"screen_name": "ReconGamesUK",
+				"location": "United Kingdom",
+				"description": "Official Twitter account for the National Reconditioning Games 2022/23. Raising the profile of deconditioning and functional decline, one gold medal at a time",
+				"url": "https://t.co/mAHEvlfbxG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mAHEvlfbxG",
+								"expanded_url": "https://future.nhs.uk/connect.ti/ECISTnetwork/view?objectID=37893328",
+								"display_url": "future.nhs.uk/connect.ti/ECI…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 439,
+				"friends_count": 29,
+				"listed_count": 3,
+				"created_at": "Wed Aug 31 15:29:46 +0000 2022",
+				"favourites_count": 337,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 276,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1565001052704972801/abDfmT0z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1565001052704972801/abDfmT0z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1564998785968791559/1667230965",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1580239660210876400,
+			"quoted_status_id_str": "1580239660210876418",
+			"quoted_status_permalink": {
+				"url": "https://t.co/H2FgBO2B15",
+				"expanded": "https://twitter.com/recongamesuk/status/1580239660210876418",
+				"display": "twitter.com/recongamesuk/s…"
+			},
+			"retweet_count": 5,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Let's make a difference across the Midlands #ReconditionTheNation Have you signed the Midlands pledge? https://t.co/TbmdvJciHc 🥇🥈🥉 @ReconGamesUK @NHSMidlands @HelenBayley3 @miriam_coffie @beardedPT @ImogenStaveley https://t.co/kthnArSVyv"
+	}
+]
