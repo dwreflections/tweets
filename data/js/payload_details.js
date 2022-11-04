c62c5f93e091559d74df09466aaeb9f34a0e9a2b
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-11-04 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7954,
-	"created_at": "Wed Nov 02 01:17:37 +0000 2022",
+	"tweets": 7955,
+	"created_at": "Fri Nov 04 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,383 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Thu Nov 03 12:40:14 +0000 2022",
+		"id": 1588132252738883600,
+		"id_str": "1588149047734460417",
+		"full_text": "Congratulations to all the @east_mids_rfca and @WMRFCA Employer Recognition Scheme Gold award winners, who received their award last night! 🎉\n\n#GoldERS22 https://t.co/k6O250nKTs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoldERS22",
+					"indices": [
+						143,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "east_mids_rfca",
+					"name": "East Midlands RFCA",
+					"id": 96968207,
+					"id_str": "96968207",
+					"indices": [
+						27,
+						42
+					]
+				},
+				{
+					"screen_name": "WMRFCA",
+					"name": "West Midland Reserve Forces & Cadets Association",
+					"id": 1944419245,
+					"id_str": "1944419245",
+					"indices": [
+						47,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/k6O250nKTs",
+					"expanded_url": "https://twitter.com/east_mids_rfca/status/1587902869990264832",
+					"display_url": "twitter.com/east_mids_rfca…",
+					"indices": [
+						154,
+						177
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2841787607,
+			"id_str": "2841787607",
+			"name": "Defence Relationship Management",
+			"screen_name": "DRM_Support",
+			"location": "United Kingdom",
+			"description": "Connecting the Ministry of Defence and the Armed Forces to UK employers.",
+			"url": "https://t.co/YgCfA0FVpJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YgCfA0FVpJ",
+							"expanded_url": "https://www.gov.uk/government/groups/defence-relationship-management",
+							"display_url": "gov.uk/government/gro…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5773,
+			"friends_count": 517,
+			"listed_count": 70,
+			"created_at": "Thu Oct 23 08:39:25 +0000 2014",
+			"favourites_count": 8297,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6535,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1587032551054614528/VkS2yoEn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1587032551054614528/VkS2yoEn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2841787607/1663680901",
+			"profile_link_color": "00616C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1587902869990264800,
+		"quoted_status_id_str": "1587902869990264832",
+		"quoted_status_permalink": {
+			"url": "https://t.co/k6O250nKTs",
+			"expanded": "https://twitter.com/east_mids_rfca/status/1587902869990264832",
+			"display": "twitter.com/east_mids_rfca…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 02 20:22:00 +0000 2022",
+			"id": 1587902869990264800,
+			"id_str": "1587902869990264832",
+			"full_text": "#GoldERS22 Award’s underway @Nat_Mem_Arb with @WMRFCA. Huge congratulations to all our winners! https://t.co/6ROj68qY5z",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				95
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GoldERS22",
+						"indices": [
+							0,
+							10
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Nat_Mem_Arb",
+						"name": "National Memorial Arboretum",
+						"id": 41090456,
+						"id_str": "41090456",
+						"indices": [
+							28,
+							40
+						]
+					},
+					{
+						"screen_name": "WMRFCA",
+						"name": "West Midland Reserve Forces & Cadets Association",
+						"id": 1944419245,
+						"id_str": "1944419245",
+						"indices": [
+							46,
+							53
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1587902641706832000,
+						"id_str": "1587902641706831873",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1587902641706831873/pu/img/Q8SB7wfolqQzwUMC.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1587902641706831873/pu/img/Q8SB7wfolqQzwUMC.jpg",
+						"url": "https://t.co/6ROj68qY5z",
+						"display_url": "pic.twitter.com/6ROj68qY5z",
+						"expanded_url": "https://twitter.com/east_mids_rfca/status/1587902869990264832/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1587902641706832000,
+						"id_str": "1587902641706831873",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1587902641706831873/pu/img/Q8SB7wfolqQzwUMC.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1587902641706831873/pu/img/Q8SB7wfolqQzwUMC.jpg",
+						"url": "https://t.co/6ROj68qY5z",
+						"display_url": "pic.twitter.com/6ROj68qY5z",
+						"expanded_url": "https://twitter.com/east_mids_rfca/status/1587902869990264832/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 23786,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1587902641706831873/pu/pl/9-X2Aes4OtFREQHD.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1587902641706831873/pu/vid/480x270/8HMuI_k0NGzH4aDS.mp4?tag=12"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1587902641706831873/pu/vid/1280x720/krBRS68_s5LdEfa_.mp4?tag=12"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1587902641706831873/pu/vid/640x360/sWFQqLwjRU5Eqwqx.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 96968207,
+				"id_str": "96968207",
+				"name": "East Midlands RFCA",
+				"screen_name": "east_mids_rfca",
+				"location": "East Midlands, UK",
+				"description": "East Midlands RFCA promotes the Reserve Forces & Cadets in the region.  We manage estates & employer engagement, to ensure Armed Forces personnel are supported.",
+				"url": "https://t.co/7qs5Jn2h8P",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7qs5Jn2h8P",
+								"expanded_url": "http://www.eastmidlandsrfca.co.uk",
+								"display_url": "eastmidlandsrfca.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3078,
+				"friends_count": 1312,
+				"listed_count": 43,
+				"created_at": "Tue Dec 15 12:04:53 +0000 2009",
+				"favourites_count": 6928,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 9057,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1572508002036449280/bB8Uqptr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1572508002036449280/bB8Uqptr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/96968207/1663750100",
+				"profile_link_color": "CEA340",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "434142",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations to all the @east_mids_rfca and @WMRFCA Employer Recognition Scheme Gold award winners, who received their award last night! 🎉\n\n#GoldERS22 https://t.co/k6O250nKTs"
+	},
+	{
 		"created_at": "Tue Nov 01 17:42:47 +0000 2022",
 		"id": 1587497328113500200,
 		"id_str": "1587500414899032065",
