54b497afbeaa1108b6f92f6fd455498cb4a5ac86
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-11-26 01:17:30 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7975,
-	"created_at": "Fri Nov 25 01:17:29 +0000 2022",
+	"tweets": 7976,
+	"created_at": "Sat Nov 26 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,310 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 25 19:30:23 +0000 2022",
+		"id": 1596224800187637800,
+		"id_str": "1596224800187637761",
+		"full_text": "Thank you to @cityoflondon @PlumbersCompany. An honour to be awarded the The Freedom of the City of London. https://t.co/8FXnt2SeWB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cityoflondon",
+					"name": "City of London",
+					"id": 16182706,
+					"id_str": "16182706",
+					"indices": [
+						13,
+						26
+					]
+				},
+				{
+					"screen_name": "PlumbersCompany",
+					"name": "Plumbers Company",
+					"id": 817892785,
+					"id_str": "817892785",
+					"indices": [
+						27,
+						43
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1596224774493163500,
+					"id_str": "1596224774493163520",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/Fibt-alX0AAf9KY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fibt-alX0AAf9KY.jpg",
+					"url": "https://t.co/8FXnt2SeWB",
+					"display_url": "pic.twitter.com/8FXnt2SeWB",
+					"expanded_url": "https://twitter.com/beardedPT/status/1596224800187637761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1596224774493163500,
+					"id_str": "1596224774493163520",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/Fibt-alX0AAf9KY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fibt-alX0AAf9KY.jpg",
+					"url": "https://t.co/8FXnt2SeWB",
+					"display_url": "pic.twitter.com/8FXnt2SeWB",
+					"expanded_url": "https://twitter.com/beardedPT/status/1596224800187637761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1596224774467948500,
+					"id_str": "1596224774467948546",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/Fibt-afXEAIRypg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fibt-afXEAIRypg.jpg",
+					"url": "https://t.co/8FXnt2SeWB",
+					"display_url": "pic.twitter.com/8FXnt2SeWB",
+					"expanded_url": "https://twitter.com/beardedPT/status/1596224800187637761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1596224774459515000,
+					"id_str": "1596224774459514881",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/Fibt-adWYAEJnnt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fibt-adWYAEJnnt.jpg",
+					"url": "https://t.co/8FXnt2SeWB",
+					"display_url": "pic.twitter.com/8FXnt2SeWB",
+					"expanded_url": "https://twitter.com/beardedPT/status/1596224800187637761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1596224774488952800,
+					"id_str": "1596224774488952833",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/Fibt-akXkAEtrdF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fibt-akXkAEtrdF.jpg",
+					"url": "https://t.co/8FXnt2SeWB",
+					"display_url": "pic.twitter.com/8FXnt2SeWB",
+					"expanded_url": "https://twitter.com/beardedPT/status/1596224800187637761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "NHS Regional Clinical Leadership Fellow | #DigitalAHP | #AHPs | Physio | Proudly Leading @UHDB_AFN | Governor @UHDBtrust | Greyhounds",
+			"url": "https://t.co/Qd40HbmAVv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Qd40HbmAVv",
+							"expanded_url": "https://campsite.bio/davidww",
+							"display_url": "campsite.bio/davidww",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3350,
+			"friends_count": 4691,
+			"listed_count": 18,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 27151,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8591,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571964147637567500/l164EWBb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571964147637567500/l164EWBb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1658777389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you to @cityoflondon @PlumbersCompany. An honour to be awarded the The Freedom of the City of London. https://t.co/8FXnt2SeWB"
+	},
+	{
 		"created_at": "Thu Nov 24 08:42:03 +0000 2022",
 		"id": 1595330571852447700,
 		"id_str": "1595699253736775682",
