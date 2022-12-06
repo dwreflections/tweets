4b71ed87f4f91bef4acf91871c89f9b42bfb4e83
dwreflections <50467496+dwreflections@users.noreply.github.com>
2022-12-06 01:17:30 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7989,
-	"created_at": "Fri Dec 02 01:17:34 +0000 2022",
+	"tweets": 7991,
+	"created_at": "Tue Dec 06 01:17:29 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,376 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 05 19:11:05 +0000 2022",
+		"id": 1599704242331463700,
+		"id_str": "1599843820786528256",
+		"full_text": "♦️Please consider completing this short survey if you are a healthcare professional working with military veterans &amp;/ or their families ♦️https://t.co/2GIPQ0gNjU @bethclinker @icaruscharity @outpostcharity @VeteranCts @Diver67Diver @HeroesCic @Contact_AF @WWTWNorthWest https://t.co/CnvTCBnVxS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bethclinker",
+					"name": "Beth Lambert",
+					"id": 755487493265457200,
+					"id_str": "755487493265457153",
+					"indices": [
+						166,
+						178
+					]
+				},
+				{
+					"screen_name": "icaruscharity",
+					"name": "The Icarus Charity",
+					"id": 978625886944669700,
+					"id_str": "978625886944669697",
+					"indices": [
+						179,
+						193
+					]
+				},
+				{
+					"screen_name": "outpostcharity",
+					"name": "outpostcharity",
+					"id": 2300246083,
+					"id_str": "2300246083",
+					"indices": [
+						194,
+						209
+					]
+				},
+				{
+					"screen_name": "VeteranCts",
+					"name": "Op Courage Veteran’s mental health service",
+					"id": 1218191275209437200,
+					"id_str": "1218191275209437185",
+					"indices": [
+						210,
+						221
+					]
+				},
+				{
+					"screen_name": "Diver67Diver",
+					"name": "Martin Diver 🏳️‍🌈",
+					"id": 504930163,
+					"id_str": "504930163",
+					"indices": [
+						222,
+						235
+					]
+				},
+				{
+					"screen_name": "HeroesCic",
+					"name": "Healthier Heroes CIC",
+					"id": 999305152204017700,
+					"id_str": "999305152204017664",
+					"indices": [
+						236,
+						246
+					]
+				},
+				{
+					"screen_name": "Contact_AF",
+					"name": "Contact Armed Forces",
+					"id": 732106996212768800,
+					"id_str": "732106996212768768",
+					"indices": [
+						247,
+						258
+					]
+				},
+				{
+					"screen_name": "WWTWNorthWest",
+					"name": "WWTWNorthWest",
+					"id": 910140281974087700,
+					"id_str": "910140281974087681",
+					"indices": [
+						259,
+						273
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2GIPQ0gNjU",
+					"expanded_url": "https://chester.onlinesurveys.ac.uk/perspectives-of-healthcare-professionals-working-with-vete-2",
+					"display_url": "chester.onlinesurveys.ac.uk/perspectives-o…",
+					"indices": [
+						142,
+						165
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1599704232151715800,
+					"id_str": "1599704232151715841",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNKhgcXkAE_O1C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNKhgcXkAE_O1C.jpg",
+					"url": "https://t.co/CnvTCBnVxS",
+					"display_url": "pic.twitter.com/CnvTCBnVxS",
+					"expanded_url": "https://twitter.com/_Kate_Salem/status/1599704242331463681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1169,
+							"h": 1636,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1599704232151715800,
+					"id_str": "1599704232151715841",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNKhgcXkAE_O1C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNKhgcXkAE_O1C.jpg",
+					"url": "https://t.co/CnvTCBnVxS",
+					"display_url": "pic.twitter.com/CnvTCBnVxS",
+					"expanded_url": "https://twitter.com/_Kate_Salem/status/1599704242331463681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1169,
+							"h": 1636,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884510068871508000,
+			"id_str": "884510068871507970",
+			"name": "Kate Salem",
+			"screen_name": "_Kate_Salem",
+			"location": "",
+			"description": "Researcher @UoCVeterans | Psychology Learning Facilitator | PhD- MH & help seeking in military families @UoCPsych",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 308,
+			"listed_count": 0,
+			"created_at": "Mon Jul 10 20:30:08 +0000 2017",
+			"favourites_count": 623,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 132,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1246937046192709632/_2fMd6Tf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1246937046192709632/_2fMd6Tf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884510068871507970/1628854436",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "♦️Please consider completing this short survey if you are a healthcare professional working with military veterans &amp;/ or their families ♦️https://t.co/2GIPQ0gNjU @bethclinker @icaruscharity @outpostcharity @VeteranCts @Diver67Diver @HeroesCic @Contact_AF @WWTWNorthWest https://t.co/CnvTCBnVxS"
+	},
+	{
+		"created_at": "Mon Dec 05 18:56:48 +0000 2022",
+		"id": 1599840225899208700,
+		"id_str": "1599840225899208704",
+		"full_text": "@DigitalKway @HelenHands_",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DigitalKway",
+					"name": "Kwesi Afful",
+					"id": 4163596217,
+					"id_str": "4163596217",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "HelenHands_",
+					"name": "Helen Hands",
+					"id": 1509274206789382100,
+					"id_str": "1509274206789382144",
+					"indices": [
+						13,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1599756415576379400,
+		"in_reply_to_status_id_str": "1599756415576379392",
+		"in_reply_to_user_id": 4163596217,
+		"in_reply_to_user_id_str": "4163596217",
+		"in_reply_to_screen_name": "DigitalKway",
+		"user": {
+			"id": 3377750235,
+			"id_str": "3377750235",
+			"name": "David Williams",
+			"screen_name": "beardedPT",
+			"location": "UHDB 🏥 / Midlands 🏡 / UoB 📚",
+			"description": "NHS Regional Clinical Leadership Fellow | Proudly Leading @UHDB_AFN | #DigitalAHP | Physio | Governor @UHDBtrust | Greyhounds",
+			"url": "https://t.co/LyQhimJMLZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LyQhimJMLZ",
+							"expanded_url": "https://fmlm.ac.uk/david-williams",
+							"display_url": "fmlm.ac.uk/david-williams",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3354,
+			"friends_count": 4699,
+			"listed_count": 19,
+			"created_at": "Wed Jul 15 20:16:42 +0000 2015",
+			"favourites_count": 27189,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8610,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571964147637567500/l164EWBb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571964147637567500/l164EWBb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3377750235/1658777389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "qam",
+		"text": "@DigitalKway @HelenHands_"
+	},
+	{
 		"created_at": "Thu Dec 01 18:16:41 +0000 2022",
 		"id": 1598379228466188300,
 		"id_str": "1598380582362353672",
